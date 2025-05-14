5a6fa333357e37609875ef0cfa697d6a976bc4ce
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-05-14 19:40:15 +0200

    Updated changed imports of App and GlobalStyles in index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
-import { App } from './App';
-import { GlobalStyles } from './GlobalStyles';
+import { App } from './core/App/index';
+import { GlobalStyles } from './core/GlobalStyles';
 
 const root = ReactDOM.createRoot(document.getElementById('root')!);
 root.render(
