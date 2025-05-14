db431b39e3d036b4b0c6b6ec79e3e92ad921c397
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-05-14 18:34:15 +0200

    Removed extraContent prop, added new optional props actorDetails and movieDetails. Both of them contain unique properties. Created interface ProductionCountry and assigned it to the productionCountries property.

--- src/common/components/Tile/index.tsx
+++ src/common/components/Tile/index.tsx
@@ -1,26 +1,39 @@
+import { StyledTile } from "./styled";
+
 const genresIds = [28, 12, 16, 35, 80, 99, 18, 10751, 14, 36, 27, 10402, 9648, 10749, 878, 10770, 53, 10752, 37] as const;
 
 type GenresIds = typeof genresIds;
 
+interface ProductionCountry {
+    iso_3166_1: string;
+    name: string;
+};
 interface TileProps {
     id: number;
     picture: string;
     title: string;
     subTitle?: string | number;
-    extraContent?: {
+    horizontalLayout?: boolean;
+    verticalLayout?: boolean;
+    movieDetails?: {
+        productionCountries: ProductionCountry[];
+        release_date: string;
         genresIds: GenresIds;
         rate: number;
         votesTotal: number;
         overview: string;
     };
-    horizontalLayout?: boolean;
-    verticalLayout?: boolean;
+    actorDetails?: {
+        biography: string;
+        birthday: string;
+        place_of_birth: string;
+    }
 };
 
-export const Tile = ({ id, picture, title, subTitle, extraContent, horizontalLayout, verticalLayout }: TileProps) => {
+export const Tile = ({ id, picture, title, subTitle, horizontalLayout, verticalLayout, movieDetails }: TileProps) => {
     return (
-        <>
-
-        </>
+        <StyledTile>
+            
+        </StyledTile>
     );
 };
