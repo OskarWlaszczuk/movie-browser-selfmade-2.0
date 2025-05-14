8784eecf48ff0b18458e17a24135ba4669d544c0
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-05-14 19:59:06 +0200

    Wrapped App and GlobalStyles into ThemeProvider

--- src/core/App/index.tsx
+++ src/core/App/index.tsx
@@ -1,10 +1,17 @@
-import { ThemeProvider } from "styled-components";
-import { theme } from "../theme";
+import { HashRouter, Route, Routes } from "react-router-dom";
 
 export const App = () => {
   return (
-    <ThemeProvider theme={theme}>
-      <></>
-    </ThemeProvider>
+    <HashRouter>
+      <Routes>
+        {/* <Route path={toMoviesSearch()} element={<SearchProcess />} />
+        <Route path={toActorSearch()} element={<SearchProcess />} />
+        <Route path={toMovie()} element={<MovieDetailsPage />} />
+        <Route path={toPerson()} element={<ActorsData />} />
+        <Route path={toMoviesList()} element={<MoviesListPage />} />
+        <Route path={toActorsList()} element={<ActorsList />} />
+        <Route path="*" element={<Navigate to={toMoviesList()} />} /> */}
+      </Routes>
+    </HashRouter>
   );
 };
