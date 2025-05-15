973a9226298f9a1d696753683aca926a1733145c
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-05-15 19:16:58 +0200

    Added missing yield key word in catch block, assigned SagaIterator to the fetchGenresHandler

--- src/genresSaga.ts
+++ src/genresSaga.ts
@@ -2,14 +2,15 @@ import { call, put, takeLatest } from "redux-saga/effects";
 import { fetchGenres, handleGenresFailed, setFetchedGenres } from "./genresSlice";
 import { GenreResponse } from "./common/aliases/types/GenresIds";
 import { fetchFromAPI } from "./common/functions/fetchFromAPI";
+import { SagaIterator } from "redux-saga";
 
-function* fetchGenresHandler() {
+function* fetchGenresHandler(): SagaIterator {
     try {
         const typedFetch = fetchFromAPI<GenreResponse[]>;
         const genresList: GenreResponse[] = yield call(typedFetch, "/genres.json");
         yield put(setFetchedGenres(genresList));
     } catch {
-        put(handleGenresFailed());
+        yield put(handleGenresFailed());
     };
 };
 
