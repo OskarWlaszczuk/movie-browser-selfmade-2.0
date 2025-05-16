1046d4f31fc4afd1353a6db0abd9c6191b7070f8
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-05-16 12:15:32 +0200

    Temporarily dispatch fetchGenres action inside GenresList component. Selects genres states (list and status) and subscribing genres state in store by using useAppSelector calls

--- src/common/components/GenresList/index.tsx
+++ src/common/components/GenresList/index.tsx
@@ -1,7 +1,28 @@
-import { GenresIds } from "../../aliases/types/GenresIds";
+import { useEffect } from "react";
+import { GenresIds } from "../../aliases/types/genre.types";
+import { fetchGenres, selectGenresList, selectGenresStatus } from "../../../genresSlice";
+import { useAppDispatch, useAppSelector } from "../../../reduxTypedHooks";
 
-export const GenresList = (genresIds: GenresIds) => {
-// Fetchowanie gatunków na podstawie genresIds tutaj
-
-
+interface GenresListProps {
+    genresIds: GenresIds;
 }
+
+export const GenresList = ({ genresIds }: GenresListProps) => {
+    const dispatch = useAppDispatch();
+
+    const genresList = useAppSelector(selectGenresList);
+    const genresStatus = useAppSelector(selectGenresStatus);
+
+    const filteredGenres = genresIds?.map(id => genresList?.find(genre => id === genre.id));
+
+    //Przenieść fetchowanie genres do głównego komponentu, tak aby odbywało się to tylko raz na render
+    useEffect(() => {
+        dispatch(fetchGenres())
+    }, [dispatch]);
+
+    return (
+        <>
+
+        </>
+    );
+};
