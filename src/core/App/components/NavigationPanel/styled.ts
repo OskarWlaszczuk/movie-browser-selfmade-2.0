e60fd6769dd16c0d04d2426f43588a62fdec493f
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-05-17 07:22:00 +0200

    Created initial version of StyledNavigationPanel styled-component

---
+++ src/core/App/components/NavigationPanel/styled.ts
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const StyledNavigationPanel = styled.nav`
+    color: ${({ theme }) => theme.colors.white};
+    background-color: ${({ theme }) => theme.colors.black};
+    height: 94px;
+`;
