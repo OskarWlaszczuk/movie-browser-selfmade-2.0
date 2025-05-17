7a734d083edb7c35ff196f7b1dd78d04e329a8b6
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-05-17 19:37:25 +0200

    Removed commandend code, replaced ROUTES object that contained properties with routes object that contains methods.

--- src/core/App/index.tsx
+++ src/core/App/index.tsx
@@ -1,29 +1,17 @@
 import { HashRouter, Navigate, Route, Routes } from "react-router-dom";
-import { GenresList } from "../../common/components/GenresList";
 import { Movies } from "../../features/ListPage/Movies";
 import { NavigationPanel } from "./components/NavigationPanel";
-import { ROUTES, toListPage } from "../../common/functions/routes";
+import { routes } from "../../common/functions/routes";
 import { People } from "../../features/ListPage/People";
 
 export const App = () => {
   return (
-    // <HashRouter>
-    //   <Routes>
-    //     {/* <Route path={toMoviesSearch()} element={<SearchProcess />} />
-    //     <Route path={toActorSearch()} element={<SearchProcess />} />
-    //     <Route path={toMovie()} element={<MovieDetailsPage />} />
-    //     <Route path={toPerson()} element={<ActorsData />} />
-    //     <Route path={toMoviesList()} element={<MoviesListPage />} />
-    //     <Route path={toActorsList()} element={<ActorsList />} />
-    //     <Route path="*" element={<Navigate to={toMoviesList()} />} /> */}
-    //   </Routes>
-    // </HashRouter>
     <HashRouter>
       <NavigationPanel />
       <Routes>
-        <Route path={ROUTES.list.movies} element={<Movies />} />
-        <Route path={ROUTES.list.people} element={<People />} />
-        <Route path="*" element={<Navigate to={ROUTES.list.movies} />} />
+        <Route path={routes.movies()} element={<Movies />} />
+        <Route path={routes.people()} element={<People />} />
+        <Route path="*" element={<Navigate to={routes.movies()} />} />
       </Routes>
     </HashRouter>
   );
