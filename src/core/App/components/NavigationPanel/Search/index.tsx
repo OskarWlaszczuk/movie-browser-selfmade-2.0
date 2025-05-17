b5379972f5a20467f241e034e3a5cef8de7e0bfc
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-05-17 20:19:38 +0200

    Update Search component to dynamically set placeholder based on search category

--- src/core/App/components/NavigationPanel/Search/index.tsx
+++ src/core/App/components/NavigationPanel/Search/index.tsx
@@ -1,11 +1,15 @@
+import { useLocation } from "react-router-dom";
 import { SearchPanel, StyledSearchIcon, Input } from "./styled";
+import { routes } from "../../../../../common/functions/routes";
 
 export const Search = () => {
+    const { pathname } = useLocation();
+    const searchCategory = pathname === routes.movies() ? "movies" : "people";
 
     return (
         <SearchPanel>
             <StyledSearchIcon />
-            <Input placeholder="Search for movies.." />
+            <Input placeholder={`Search for ${searchCategory}..`} />
         </SearchPanel>
     );
 };
