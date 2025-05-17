7a68ea961a0db4f306d89101fea1c7d54a1cc47c
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-05-17 07:21:32 +0200

    Created first Routes for page navigation. Added NavigationPanel outside the Routes element to display it all the time.Set movie details page as the homepage

--- src/core/App/index.tsx
+++ src/core/App/index.tsx
@@ -1,16 +1,27 @@
-import { HashRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, Navigate, Route, Routes } from "react-router-dom";
+import { GenresList } from "../../common/components/GenresList";
+import { Movies } from "../../features/ListPage/Movies";
+import { NavigationPanel } from "./components/NavigationPanel";
+import { ROUTES, toListPage } from "../../common/functions/routes";
 
 export const App = () => {
   return (
+    // <HashRouter>
+    //   <Routes>
+    //     {/* <Route path={toMoviesSearch()} element={<SearchProcess />} />
+    //     <Route path={toActorSearch()} element={<SearchProcess />} />
+    //     <Route path={toMovie()} element={<MovieDetailsPage />} />
+    //     <Route path={toPerson()} element={<ActorsData />} />
+    //     <Route path={toMoviesList()} element={<MoviesListPage />} />
+    //     <Route path={toActorsList()} element={<ActorsList />} />
+    //     <Route path="*" element={<Navigate to={toMoviesList()} />} /> */}
+    //   </Routes>
+    // </HashRouter>
     <HashRouter>
+      <NavigationPanel />
       <Routes>
-        {/* <Route path={toMoviesSearch()} element={<SearchProcess />} />
-        <Route path={toActorSearch()} element={<SearchProcess />} />
-        <Route path={toMovie()} element={<MovieDetailsPage />} />
-        <Route path={toPerson()} element={<ActorsData />} />
-        <Route path={toMoviesList()} element={<MoviesListPage />} />
-        <Route path={toActorsList()} element={<ActorsList />} />
-        <Route path="*" element={<Navigate to={toMoviesList()} />} /> */}
+        <Route path={ROUTES.list.movies} element={<Movies />} />
+        <Route path="*" element={<Navigate to={ROUTES.details.movie} />} />
       </Routes>
     </HashRouter>
   );
