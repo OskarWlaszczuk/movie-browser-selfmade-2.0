2c14583eeb783e3c6b4e3be9e531a84629228d18
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-05-17 07:45:41 +0200

    Refactor StyledNavigationPanel component and add NavItemsList and NavItem styled components

--- src/core/App/components/NavigationPanel/styled.ts
+++ src/core/App/components/NavigationPanel/styled.ts
@@ -1,7 +1,34 @@
+import { NavLink } from "react-router-dom";
 import styled from "styled-components";
 
-export const StyledNavigationPanel = styled.nav`
+export const StyledNavPanel = styled.nav`
     color: ${({ theme }) => theme.colors.white};
     background-color: ${({ theme }) => theme.colors.black};
     height: 94px;
+`;
+
+export const NavItemsList = styled.div`
+    display: flex;
+    gap: 16px;
+`;
+
+export const NavItem = styled(NavLink)`
+    color: ${({ theme }) => theme.colors.white};
+    text-decoration: none;
+    font-size: 14px;
+    font-weight: 600;
+    border: 1px solid transparent;
+    padding: 12px 24px;
+    transition: 0.5s;
+     border-radius: 24px;
+
+    &:hover {
+        border: 1px solid ${({ theme }) => theme.colors.white};
+        transform: scale(1.05);
+    }
+    
+    &.active {
+        border: 1px solid ${({ theme }) => theme.colors.white};
+        border-radius: 24px;
+    };
 `;
