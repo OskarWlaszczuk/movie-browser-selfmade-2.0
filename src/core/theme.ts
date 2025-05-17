a84534a94e645f3db352d75aa2a88f853b53a3fc
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-05-17 17:28:03 +0200

    Add additional color definitions to theme

--- src/core/theme.ts
+++ src/core/theme.ts
@@ -3,5 +3,13 @@ export const theme = {
         white: "#FFFFFF",
         black: "#18181B",
         waterloo: "#7E839A",
+        mystic: "#E4E6F0",
+        woodsmoke: "#18181B",
+        whisper: "#F5F5FA",
+        silver: "#C4C4C4",
+        brightHeather: "#bac7d580",
+        stormGray: "#74788B",
+        pattensBlue: "#D6E4FF",
+        scienceBlue: "#0044CC",
     },
 } as const;
