bd7a4aeef19f4a0c1741e650ae4112b49c5167b6
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-05-17 19:43:22 +0200

    Fixed import of SearchIcon

--- src/core/App/components/NavigationPanel/Search/styled.ts
+++ src/core/App/components/NavigationPanel/Search/styled.ts
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import { ReactComponent as SearchIcon } from "..Search.svg";
+import { ReactComponent as SearchIcon } from "../Search.svg";
 
 export const SearchPanel = styled.div`
     display: flex;
