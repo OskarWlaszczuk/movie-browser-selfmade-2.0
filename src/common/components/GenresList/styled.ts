800702944169f7d213a4cb21f3331661c71efd0c
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-05-17 22:12:07 +0200

    Refactor StyledGenres component to use 'ul' instead of 'li' and adjust styles for improved layout and consistency

--- src/common/components/GenresList/styled.ts
+++ src/common/components/GenresList/styled.ts
@@ -1,5 +1,18 @@
 import styled from "styled-components";
 
-export const StyledGenres = styled.li`
+export const StyledGenres = styled.ul`
+    display: flex;
+    gap: 8px;
+    flex-wrap: wrap;
+    justify-items: start;
+`;
 
+export const Genre = styled.li`
+    color: ${({ theme }) => theme.colors.black};
+    background-color: ${({ theme }) => theme.colors.mystic};
+    padding: 8px 16px;
+    list-style: none;
+    border-radius: 5px;
+    flex-grow: 1;
+    text-align: center;
 `;
