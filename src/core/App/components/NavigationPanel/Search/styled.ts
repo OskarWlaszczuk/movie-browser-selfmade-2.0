1ebd3f43433af0f5736ce67ccfe8514a95d93cb5
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-05-17 19:56:56 +0200

    Refactor Search component styles and update import path for SearchIcon

--- src/core/App/components/NavigationPanel/Search/styled.ts
+++ src/core/App/components/NavigationPanel/Search/styled.ts
@@ -1,31 +1,27 @@
 import styled from "styled-components";
-import { ReactComponent as SearchIcon } from "../Search.svg";
+import { ReactComponent as SearchIcon } from "./Icons/Search.svg";
 
 export const SearchPanel = styled.div`
     display: flex;
     flex-basis: 30%;
     align-items: center;
     gap: 16px;
-    padding: 12px;
-    padding-right: 16px;
-    
+    padding: 12px 22px;
     border: 1px solid ${({ theme }) => theme.colors.mystic};
     border-radius: 33px;
     background-color: ${({ theme }) => theme.colors.white};
 `;
 
 export const StyledSearchIcon = styled(SearchIcon)`
-    margin-left: 12px;
     max-width: 40px;
 `;
 
 export const Input = styled.input`
     border: none;
-    background-color: unset;
+    background-color: transparent;
     font-size: 16px;
     font-weight: 400;
     line-height: 24px;
-    width: 100%;
     
     &::placeholder {
         color: ${({ theme }) => theme.colors.waterloo};
