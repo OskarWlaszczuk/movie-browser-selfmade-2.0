435407db43e26fb82abcfeba2bc132f9acbd5f06
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-05-17 19:57:57 +0200

    Add Search component with styled icon and input field

--- src/core/App/components/NavigationPanel/Search/index.tsx
+++ src/core/App/components/NavigationPanel/Search/index.tsx
@@ -1,3 +1,11 @@
+import { SearchPanel, StyledSearchIcon, Input } from "./styled";
+
 export const Search = () => {
 
+    return (
+        <SearchPanel>
+            <StyledSearchIcon />
+            <Input placeholder="Search for movies.." />
+        </SearchPanel>
+    );
 };
