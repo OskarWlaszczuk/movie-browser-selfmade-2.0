7bf6a88b9e2d180ddd5511ad1ba6222f9d0c4f30
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-05-18 20:39:52 +0200

    Added SectionHeader component and passed title as text prop

--- src/features/ListPage/index.tsx
+++ src/features/ListPage/index.tsx
@@ -1,15 +1,21 @@
 import { FetchStatus } from "../../common/aliases/types/FetchStatus";
+import { Main } from "../../common/components/Main";
+import { SectionHeader } from "../../common/components/SectionHeader";
 
 interface ListPageProps {
     title: string;
-    list:any[];
+    list: any[];
     fetchStatuses: FetchStatus[]
 }
 
 export const ListPage = ({ title, list, fetchStatuses }: ListPageProps) => {
 
     return (
+        // <Main
+
+        // />
         <>
+            <SectionHeader text={title} isMainHeader={false} />
         </>
-    )
+    );
 };
