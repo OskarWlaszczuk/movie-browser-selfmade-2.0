3073919c7a8432496d993b7a9d382cd59ec8de04
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-05-18 20:40:10 +0200

    Added popularMovies state to the store

--- src/core/store.ts
+++ src/core/store.ts
@@ -2,12 +2,14 @@ import { configureStore } from '@reduxjs/toolkit';
 import createSagaMiddleware from "redux-saga"
 import { rootSaga } from './rootSaga';
 import { genresReducer } from '../genresSlice';
+import { popularMoviesReducer } from '../popularMoviesSlice';
 
 const sagaMiddleware = createSagaMiddleware();
 
 export const store = configureStore({
     reducer: {
         genres: genresReducer,
+        popularMovies: popularMoviesReducer,
     },
     middleware: (getDefaultMiddleware) =>
         getDefaultMiddleware().concat(sagaMiddleware),
