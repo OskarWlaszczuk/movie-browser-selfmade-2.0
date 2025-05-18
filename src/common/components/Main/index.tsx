d8512e87e7447a205e738b9615ea05e68a44b0e1
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-05-18 19:37:17 +0200

    Created Main component. Depeneding on passed currentFetchStatus prop, displays specif content based on fetch status

---
+++ src/common/components/Main/index.tsx
@@ -0,0 +1,34 @@
+import { ReactElement } from "react";
+import { FetchStatus } from "../../aliases/types/FetchStatus";
+import { FETCH_STATUSES } from "../../constants/FETCH_STATUSES";
+import { MainContent, MainSection } from "./styled";
+
+interface MainProps {
+    content: ReactElement;
+    currentFetchStatus: FetchStatus;
+    bannerContent?: ReactElement;
+};
+
+export const Main = ({ content, bannerContent, currentFetchStatus }: MainProps) => {
+    //Ustawić poprawne elementy zwracane w razie ładowania/błędu/initial
+    switch (currentFetchStatus) {
+        case FETCH_STATUSES.SUCCESS:
+            return (
+                <MainSection>
+                    {bannerContent || <></>}
+                    <MainContent>
+                        {content}
+                    </MainContent>
+                </MainSection>
+            );
+
+        case FETCH_STATUSES.LOADING:
+            return <>Ładowanie</>;
+
+        case FETCH_STATUSES.FAILED:
+            return <>Błąd</>;
+
+        default:
+            return <>Initial</>;
+    };
+};
