13c91edc89fb9079b8b72f5b50b017539a6dc1da
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-05-18 19:40:55 +0200

    Created ListPage generic component that will be used for building structure of list-type pages. Passed it title, list and fetchStatuses props

---
+++ src/features/ListPage/index.tsx
@@ -0,0 +1,15 @@
+import { FetchStatus } from "../../common/aliases/types/FetchStatus";
+
+interface ListPageProps {
+    title: string;
+    list:any[];
+    fetchStatuses: FetchStatus[]
+}
+
+export const ListPage = ({ title, list, fetchStatuses }: ListPageProps) => {
+
+    return (
+        <>
+        </>
+    )
+};
