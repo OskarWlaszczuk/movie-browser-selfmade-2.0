37d1b57dc5401f63157e09cf9216749fb06bcbc8
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-05-18 08:50:27 +0200

    Moved GenresStatus to Movies

--- src/common/components/GenresList/index.tsx
+++ src/common/components/GenresList/index.tsx
@@ -1,5 +1,5 @@
 import { GenresIds } from "../../aliases/types/genre.types";
-import { selectGenresList, selectGenresStatus } from "../../../genresSlice";
+import { selectGenresList } from "../../../genresSlice";
 import { useAppSelector } from "../../../reduxTypedHooks";
 import { Genre, StyledGenres } from "./styled";
 
@@ -8,9 +8,7 @@ interface GenresListProps {
 }
 
 export const GenresList = ({ genresIds }: GenresListProps) => {
-
     const genresList = useAppSelector(selectGenresList);
-    const genresStatus = useAppSelector(selectGenresStatus);
 
     const extractedGenres = genresIds.map(id => genresList?.find(genre => id === genre.id));
     return (
