f3b7e388321778e6806bcd733124a13ef7ba9a3f
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-05-18 19:27:05 +0200

    Add styled component for TilesList with grid layout

---
+++ src/features/ListPage/Movies/styled.ts
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const TilesList = styled.section`
+    display: grid;
+    grid-template-columns: repeat(auto-fill, minmax(288px, 1fr));
+    grid-gap: 24px;
+    justify-content: center;
+`;
