efbaea7322f5a57485b44f240c324f329b1f4cc8
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-05-20 08:34:00 +0200

    Refactor Movies component to remove unused state and API call, integrating popular movies into ListPage

--- src/features/ListPage/Movies/index.js
+++ src/features/ListPage/Movies/index.js
@@ -1,64 +1,29 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { useAppDispatch, useAppSelector } from "../../../reduxTypedHooks";
 import { fetchGenres, selectGenresStatus } from "../../../genresSlice";
-import { GenresList } from "../../../common/components/GenresList";
-import axios from "axios";
-import { Tile } from "../../../common/components/Tile";
-import { TilesList } from "./styled";
+import { fetchPopularMovies, selectPopularMoviesList, selectPopularMoviesStatus } from "../../../popularMoviesSlice";
+import { ListPage } from "..";
 
 export const Movies = () => {
     const dispatch = useAppDispatch();
+
     const genresStatus = useAppSelector(selectGenresStatus);
-    const [data, setData] = useState(null);
-    const [error, setError] = useState(null);
+
+    const popularMoviesStatus = useAppSelector(selectPopularMoviesStatus);
+    const popularMovies = useAppSelector(selectPopularMoviesList);
 
     useEffect(() => {
-        const fetchExample = async () => {
-            try {
-                const response = await axios.get('/popularMovies.json');
-                setData(response.data);
-            } catch (err) {
-                setError(err.message);
-            }
-        };
-
-        fetchExample();
-    }, []);
-
-    console.log(data)
-    useEffect(() => {
-        dispatch(fetchGenres())
+        dispatch(fetchGenres());
+        dispatch(fetchPopularMovies());
     }, [dispatch]);
 
     return (
         <>
-            Movies
-            <TilesList>
-                {
-                    data?.map(({
-                        genre_ids,
-                        id,
-                        vote_average,
-                        vote_count,
-                        title,
-                        release_date,
-                        poster_path
-                    }, index) => (
-                        <Tile
-                            key={index}
-                            id={id}
-                            picture={"poster_path"}
-                            title={title}
-                            subTitle={release_date}
-                            movieDetails={{
-                                genresIds: genre_ids,
-                                rate: vote_average,
-                                votesTotal: vote_count,
-                            }}
-                        />
-                    ))
-                }
-            </TilesList>
+            <ListPage
+                title="Popular movies"
+                list={popularMovies}
+                fetchStatuses={[genresStatus, popularMoviesStatus]}
+            />
         </>
     );
 };
