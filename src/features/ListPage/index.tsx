6a9f6486bce12e0b0e513b93152d851c31b79105
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-05-20 08:33:44 +0200

    Added temporarily commented code of map iteration on data, added TilesList component

--- src/features/ListPage/index.tsx
+++ src/features/ListPage/index.tsx
@@ -1,6 +1,7 @@
 import { FetchStatus } from "../../common/aliases/types/FetchStatus";
 import { Main } from "../../common/components/Main";
 import { SectionHeader } from "../../common/components/SectionHeader";
+import { TilesList } from "./Movies/styled";
 
 interface ListPageProps {
     title: string;
@@ -10,12 +11,38 @@ interface ListPageProps {
 
 export const ListPage = ({ title, list, fetchStatuses }: ListPageProps) => {
 
-    return (
-        // <Main
+    /* <TilesList>
+                    {
+                        data?.map(({
+                            genre_ids,
+                            id,
+                            vote_average,
+                            vote_count,
+                            title,
+                            release_date,
+                            poster_path
+                        }, index) => (
+                            <Tile
+                                key={index}
+                                id={id}
+                                picture={"poster_path"}
+                                title={title}
+                                subTitle={release_date}
+                                movieDetails={{
+                                    genresIds: genre_ids,
+                                    rate: vote_average,
+                                    votesTotal: vote_count,
+                                }}
+                            />
+                        ))
+                    }
+                </TilesList> */
 
-        // />
+    return (
         <>
-            <SectionHeader text={title} isMainHeader={false} />
+            <SectionHeader text={title} isMainHeader />
+            <TilesList>
+            </TilesList>
         </>
     );
 };
