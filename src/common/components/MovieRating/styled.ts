ad81c7a8bd4ca27b70d02239e435b7d3fcabfe14
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-05-20 21:12:15 +0200

    Add styled components for MovieRating section and star icon

---
+++ src/common/components/MovieRating/styled.ts
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+import { ReactComponent as StarIcon } from "./Icons/Star.svg";
+
+export const StyledMovieRating = styled.section`
+    display: flex;
+    gap: 12px;
+    align-items: center;
+`;
+
+export const StyledStarIcon = styled(StarIcon)`
+    width: 24px;
+`;
+
+export const RatingScore = styled.strong`
+    font-size: ${({ theme }) => theme.fontSizes.m};
+`;
