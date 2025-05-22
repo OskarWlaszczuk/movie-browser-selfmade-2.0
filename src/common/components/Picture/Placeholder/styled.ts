a8faf9a49d9a403227c66598490cacee755129b2
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-05-22 11:15:45 +0200

    Add styled components for movie and person placeholders

---
+++ src/common/components/Picture/Placeholder/styled.ts
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+import { ReactComponent as MoviePlaceholder } from "../Icons/MoviePlaceholder.svg";
+import { ReactComponent as PersonPlaceholder } from "../Icons/PersonPlaceholder.svg";
+
+export const StyledPlaceholder = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: ${({ theme }) => theme.colors.silver};
+    border-radius: 8px;
+    height: 434px;
+`;
+
+export const StyledMoviePlaceholder = styled(MoviePlaceholder)`
+
+`;
+
+export const StyledPersonPlaceholder = styled(PersonPlaceholder)`
+
+`;
