b771273ffc6984ee2a3effcd06c7bf71701d0a47
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-05-22 21:40:16 +0200

    Changed MainContent display to flex and set its elements direction to column, decreased width to 90% and added margin to center content

--- src/common/components/Main/styled.ts
+++ src/common/components/Main/styled.ts
@@ -6,8 +6,8 @@ export const MainSection = styled.main`
 
 export const MainContent = styled.section`
     background-color: ${({ theme }) => theme.colors.whisper};
-    width: 100%;
-    display: grid;
-    grid-gap: 64px;
-    padding: 20px;
+    margin: 20px auto 0;
+    display: flex;
+    flex-direction: column;
+    width: 90%;
 `;
