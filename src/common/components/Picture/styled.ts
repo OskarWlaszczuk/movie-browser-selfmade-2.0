57413fdb2dda4b83692f1c4e67607d4a802633be
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-05-22 11:26:26 +0200

    Refactor StyledPicture component to use img tag and remove unnecessary props and styles

--- src/common/components/Picture/styled.ts
+++ src/common/components/Picture/styled.ts
@@ -1,16 +1,5 @@
 import styled from "styled-components";
 
-interface StyledPictureProps {
-    $picture: string;
-}
-
-export const StyledPicture = styled.div.attrs<StyledPictureProps>(({ $picture }) => ({
-    style: {
-        backgroundImage: `url(${$picture})`,
-    },
-})) <StyledPictureProps>`
-    background-size: cover;
-    background-position: center;
+export const StyledPicture = styled.img`
     border-radius: 8px;
-    padding-top: calc(100% * 585 / 582);
 `;
