974ae46f59e8df9aa9e5c2963e788544672af9fd
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-05-22 11:34:06 +0200

    Update grid column size in TilesList for improved layout responsiveness

--- src/features/ListPage/Movies/styled.ts
+++ src/features/ListPage/Movies/styled.ts
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const TilesList = styled.section`
     display: grid;
-    grid-template-columns: repeat(auto-fill, minmax(288px, 1fr));
+    grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
     grid-gap: 24px;
     justify-content: center;
 `;
