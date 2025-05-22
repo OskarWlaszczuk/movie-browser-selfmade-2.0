894d07328b88adee6561ecd37b12f5901ce943aa
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-05-22 10:43:32 +0200

    Add entityType prop to Picture component for enhanced flexibility

--- src/common/components/Picture/index.tsx
+++ src/common/components/Picture/index.tsx
@@ -6,9 +6,10 @@ import { getInvalidPictureUrl } from "./utilis/getInvalidPictureUrl";
 interface PictureProps {
     picturePath: string;
     pictureWidth: PictureWidth;
+    entityType?: "movie" | "person";
 }
 
-export const Picture = ({ picturePath, pictureWidth }: PictureProps) => {
+export const Picture = ({ picturePath, pictureWidth, entityType }: PictureProps) => {
     const pictureUrl = getPictureUrl(picturePath, pictureWidth);
 
     const isValidImageUrl = pictureUrl !== getInvalidPictureUrl(pictureWidth);
