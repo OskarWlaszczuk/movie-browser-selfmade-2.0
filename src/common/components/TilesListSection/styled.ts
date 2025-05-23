6011fe905bd3f4955fd6487595bbc1cd32726a69
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-05-23 21:35:44 +0200

    Remove commented-out code and clean up TilesList styles

--- src/common/components/TilesListSection/styled.ts
+++ src/common/components/TilesListSection/styled.ts
@@ -1,16 +1,6 @@
 import styled from "styled-components";
 
-// export const TilesListContainer = styled.section`
-//     /* display: flex;
-//     flex-direction: column;
-//     align-items: center */
-//     margin: auto;
-// `;
-
 export const TilesList = styled.div`
-    /* grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
-    gap: 24px; */
-    /* max-width: calc(4 * 340px + 3 * 24px); */
     display: grid;
     grid-gap: 24px;
     grid-template-columns: repeat(auto-fit, minmax(270px, 1fr))
