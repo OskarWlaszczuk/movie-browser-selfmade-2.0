a68f69affb62890effe9cc7e9cfa46b1dae7abcc
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-05-23 09:13:26 +0200

    Removed defalt margin for MetaData

--- src/common/components/MetaData/index.ts
+++ src/common/components/MetaData/index.ts
@@ -4,4 +4,5 @@ export const MetaData = styled.p`
     font-size: ${({ theme }) => theme.fontSizes.m};
     font-weight: ${({ theme }) => theme.fontWeights.regular};
     color: ${({ theme }) => theme.colors.waterloo};
+    margin: 0;
 `;
