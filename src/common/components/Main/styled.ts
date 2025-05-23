9977259be2033861fc8c8c8cd4233758b9ee4b22
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-05-23 09:13:09 +0200

    Moved background-color property from MainContent to root element in GlobalStyles

--- src/common/components/Main/styled.ts
+++ src/common/components/Main/styled.ts
@@ -1,11 +1,9 @@
 import styled from "styled-components";
 
 export const MainSection = styled.main`
-
 `;
 
 export const MainContent = styled.section`
-    background-color: ${({ theme }) => theme.colors.whisper};
     margin: 20px auto 0;
     display: flex;
     flex-direction: column;
--- src/core/GlobalStyles.ts
+++ src/core/GlobalStyles.ts
@@ -14,7 +14,7 @@ export const GlobalStyles = createGlobalStyle`
     };
 
     #root {
-         font-family: "Inter", sans-serif;
+        font-family: "Inter", sans-serif;
             -webkit-font-smoothing: antialiased;
             -moz-osx-font-smoothing: grayscale;
         background-color: ${({ theme }) => theme.colors.whisper};
