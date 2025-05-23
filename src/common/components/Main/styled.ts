632835a37f657ecdfedc3076f806ecb40ed14770
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-05-23 21:07:33 +0200

    Fix margin in MainContent for consistent spacing

--- src/common/components/Main/styled.ts
+++ src/common/components/Main/styled.ts
@@ -4,7 +4,7 @@ export const MainSection = styled.main`
 `;
 
 export const MainContent = styled.section`
-    margin: 20px auto 0;
+    margin: 20px auto;
     display: flex;
     flex-direction: column;
     width: 90%;
