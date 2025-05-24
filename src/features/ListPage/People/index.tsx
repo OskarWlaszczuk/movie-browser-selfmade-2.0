d8ffde840efc377b9bb19aefcfaebeebe52b86d7
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-05-24 09:53:24 +0200

    Refactor People component to use ListPage for rendering and add data fetching logic

--- src/features/ListPage/People/index.tsx
+++ src/features/ListPage/People/index.tsx
@@ -1,7 +1,23 @@
+import { useEffect } from "react";
+import { useAppDispatch, useAppSelector } from "../../../reduxTypedHooks";
+import { ListPage } from "../index";
+import { fetchPopularPeople, selectPopularPeopleList, selectPopularPeopleStatus } from "../../../popularPeopleSlice";
+
 export const People = () => {
+    const dispatch = useAppDispatch();
+
+    const popularPeopleStatus = useAppSelector(selectPopularPeopleStatus);
+    const popularPeople = useAppSelector(selectPopularPeopleList);
+
+    useEffect(() => {
+        dispatch(fetchPopularPeople());
+    }, [dispatch]);
+
     return (
-        <>
-            People
-        </>
+        <ListPage
+            title="Popular people"
+            list={popularPeople}
+            fetchStatuses={[popularPeopleStatus]}
+        />
     );
 };
