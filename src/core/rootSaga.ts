23af807fa9b9b8f2df4bc4cc60efa9bc2a28d518
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-05-24 09:50:54 +0200

    Add popularPeopleSaga to rootSaga for improved data handling

--- src/core/rootSaga.ts
+++ src/core/rootSaga.ts
@@ -1,10 +1,12 @@
 import { all } from "redux-saga/effects";
 import { genresSaga } from "../genresSaga";
 import { popularMoviesSaga } from "../popularMoviesSaga";
+import { popularPeopleSaga } from "../popularPeopleSaga";
 
 export function* rootSaga() {
     yield all([
         genresSaga(),
         popularMoviesSaga(),
+        popularPeopleSaga(),
     ]);
 };
