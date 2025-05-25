aa8a01d1ef4ae5a619f905850d66473c06ee2f32
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-05-25 21:15:50 +0200

    Add 'as const' assertion to apiUrls for improved type safety

--- src/common/constants/pictureConfigs.ts
+++ src/common/constants/pictureConfigs.ts
@@ -1,7 +1,7 @@
 export const apiUrls = {
     base: "https://api.themoviedb.org/3",
     image: "https://image.tmdb.org/t/p/"
-};
+} as const;
 
 export const pictureWidths = {
     backdrop: "original",
