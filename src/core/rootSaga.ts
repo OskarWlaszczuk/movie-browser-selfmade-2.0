4266399af098decadca22d52b63bee5d79e50c13
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-05-25 21:15:59 +0200

    Add movieCreditsSaga to rootSaga for handling movie credits side effects

--- src/core/rootSaga.ts
+++ src/core/rootSaga.ts
@@ -2,11 +2,13 @@ import { all } from "redux-saga/effects";
 import { genresSaga } from "../genresSaga";
 import { popularMoviesSaga } from "../popularMoviesSaga";
 import { popularPeopleSaga } from "../popularPeopleSaga";
+import { movieCreditsSaga } from "../movieCreditsSaga";
 
 export function* rootSaga() {
     yield all([
         genresSaga(),
         popularMoviesSaga(),
         popularPeopleSaga(),
+        movieCreditsSaga(),
     ]);
 };
