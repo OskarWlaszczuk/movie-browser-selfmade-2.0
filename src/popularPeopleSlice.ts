c0d64b1914bdd72253a050588c10c981a7336098
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-05-25 11:12:07 +0200

    Refactor popularPeopleSlice to utilize createPopularListSlice for improved state management and consistency

--- src/popularPeopleSlice.ts
+++ src/popularPeopleSlice.ts
@@ -1,56 +1,7 @@
-import { createSlice, PayloadAction } from "@reduxjs/toolkit";
-import { FetchStatus } from "./common/aliases/types/FetchStatus";
-import { FETCH_STATUSES } from "./common/constants/FETCH_STATUSES";
-import { RootState } from "./core/store";
-import { PopularPeopleResponse } from "./common/aliases/interfaces/TMDBRList";
+import { createPopularListSlice } from "./common/functions/createPopularListSlice";
+import { Person } from "./common/aliases/interfaces/Person";
 
-interface PopularPeopleState {
-    list: PopularPeopleResponse["results"];
-    currentPage: PopularPeopleResponse["page"];
-    totalPages: PopularPeopleResponse["total_pages"];
-    totalResults: PopularPeopleResponse["total_results"];
-    status: FetchStatus;
-}
-
-const initialState: PopularPeopleState = {
-    list: [],
-    currentPage: 0,
-    totalPages: 0,
-    totalResults: 0,
-    status: FETCH_STATUSES.IDLE,
-};
-
-const popularPeopleSlice = createSlice({
-    name: "popularPeople",
-    initialState,
-    reducers: {
-        fetchPopularPeople: (state) => {
-            state.status = FETCH_STATUSES.LOADING;
-        },
-        setFetchedPopularPeople: (state, { payload }: PayloadAction<PopularPeopleResponse>) => {
-            state.status = FETCH_STATUSES.SUCCESS;
-            state.list = payload.results;
-            state.currentPage = payload.page;
-            state.totalPages = payload.total_pages;
-            state.totalResults = payload.total_results;
-        },
-        handlePopularPeopleFailed: (state) => {
-            state.status = FETCH_STATUSES.FAILED;
-        },
-    },
-});
-
-export const {
-    fetchPopularPeople,
-    setFetchedPopularPeople,
-    handlePopularPeopleFailed,
-} = popularPeopleSlice.actions;
-
-export const selectPopularPeople = (state: RootState) => state.popularPeople;
-export const selectPopularPeopleList = (state: RootState) => selectPopularPeople(state).list;
-export const selectPopularPeopleStatus = (state: RootState) => selectPopularPeople(state).status;
-export const selectPopularPeopleTotalPages = (state: RootState) => selectPopularPeople(state).totalPages;
-export const selectPopularPeopleTotalResults = (state: RootState) => selectPopularPeople(state).totalResults;
-export const selectPopularPeopleCurrentPage = (state: RootState) => selectPopularPeople(state).currentPage;
-
+export const popularPeopleSlice = createPopularListSlice<Person>({ stateName: "popularPeople" });
 export const popularPeopleReducer = popularPeopleSlice.reducer;
+
+export const { actions: popularPeopleActions, selectors: popularPeopleSelectors } = popularPeopleSlice;
