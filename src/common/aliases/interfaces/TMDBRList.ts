1b01e568dc3c9e403856d656fd617b099f9a882e
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-05-25 11:13:17 +0200

    Renamed TMDBRList to PopularListApi and its generic param to PopularListItemType. Replaced 'Response' suffix in PopularPeopleResponse and PopularMoviesResponse with 'Api'

--- src/common/aliases/interfaces/TMDBRList.ts
+++ src/common/aliases/interfaces/TMDBRList.ts
@@ -1,13 +1,13 @@
 import { Movie } from "./Movie";
 import { Person } from "./Person";
 
-export interface TMDBRList<ResultType> {
+export interface PopularListApi<PopularListItemType> {
     page: number;
-    results: ResultType[];
+    results: PopularListItemType[];
     total_pages: number;
     total_results: number;
 }
 
 
-export type PopularPeopleResponse = TMDBRList<Person>;
-export type PopularMoviesResponse = TMDBRList<Movie>;
+export type PopularPeopleApi = PopularListApi<Person>;
+export type PopularMovieApi = PopularListApi<Movie>;
