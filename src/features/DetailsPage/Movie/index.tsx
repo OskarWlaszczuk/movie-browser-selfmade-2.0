dd88e9931c929d8386b6121a3f462fd713442a4a
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-05-25 21:17:05 +0200

    Add Movie component to display movie credits and details

---
+++ src/features/DetailsPage/Movie/index.tsx
@@ -0,0 +1,44 @@
+import { useParams } from "react-router-dom"
+import { DetailsPage } from "../index"
+import { useEffect } from "react";
+import { useAppDispatch, useAppSelector } from "../../../reduxTypedHooks";
+import { fetchMovieCredits, selectMovieCast, selectMovieCreditsStatus, selectMovieCrew } from "../../../movieCreditsSlice";
+import { renderPersonItem } from "../../../common/functions/renderPersonItem";
+
+export const Movie = () => {
+    const { movieId } = useParams();
+    const dispatch = useAppDispatch();
+    const crew = useAppSelector(selectMovieCrew);
+    const cast = useAppSelector(selectMovieCast);
+    const movieCreditsStatus = useAppSelector(selectMovieCreditsStatus);
+    console.log(crew, cast, movieCreditsStatus)
+
+    useEffect(() => {
+        dispatch(fetchMovieCredits({ movieId }));
+    }, [dispatch, movieId]);
+
+    const castSectionData = {
+        list: cast,
+        titleData: {
+            text: "cast",
+            isPageTitle: false,
+        },
+        renderListItem: renderPersonItem,
+    };
+
+    const crewSectionData = {
+        list: crew,
+        titleData: {
+            text: "Crew",
+            isPageTitle: false,
+        },
+        renderListItem: renderPersonItem,
+    }
+
+    return (
+        <DetailsPage
+            sectionsData={[castSectionData, crewSectionData]}
+            fetchStatuses={[movieCreditsStatus]}
+        />
+    )
+}
