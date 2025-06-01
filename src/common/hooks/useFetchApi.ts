88d692d2aeed4af43ceaef365d0f4642b44bb79f
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-06-01 21:14:42 +0200

    Implement useFetchApi hook for API data retrieval using React Query

--- src/common/hooks/useFetchApi.ts
+++ src/common/hooks/useFetchApi.ts
@@ -1,2 +1,17 @@
-export const useFetchApi = () => {
+import { useQuery } from "@tanstack/react-query";
+import { fetchFromAPI } from "../functions/fetchFromAPI";
+interface UseFetchApiInput {
+    queryKey: string;
+    url: string;
+    urlDependencies?: (string | number)[];
 }
+
+//Wyciąganie dynamicznej właściwości już wewnątrz hooka
+export const useFetchApi = <ResponseType,>({ queryKey, url, urlDependencies = [] }: UseFetchApiInput) => {
+    const { status, data } = useQuery({
+        queryKey: [queryKey, ...urlDependencies],
+        queryFn: () => fetchFromAPI<ResponseType>(url),
+    });
+
+    return { status, data };
+};
