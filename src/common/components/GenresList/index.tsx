a383722ed37208470c5054c70a1a11b675a4c26f
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-06-01 21:14:28 +0200

    Refactor GenresList to use useFetchApi for genre data retrieval

--- src/common/components/GenresList/index.tsx
+++ src/common/components/GenresList/index.tsx
@@ -1,16 +1,22 @@
-import { GenresIds } from "../../aliases/types/genre.types";
-import { selectGenresList } from "../../../genresSlice";
-import { useAppSelector } from "../../../reduxTypedHooks";
+import { GenreResponse, GenresIds } from "../../aliases/types/genre.types";
+// import { selectGenresList } from "../../../genresSlice";
+// import { useAppSelector } from "../../../reduxTypedHooks";
 import { Genre, StyledGenres } from "./styled";
+import { useFetchApi } from "../../hooks/useFetchApi";
 
 interface GenresListProps {
     genresIds: GenresIds;
 }
 
 export const GenresList = ({ genresIds }: GenresListProps) => {
-    const genresList = useAppSelector(selectGenresList);
-// @ts-ignore
-    const extractedGenres = genresIds.map(id => genresList?.find(genre => id === genre.id));
+    interface RawGenresApi {
+        genres: GenreResponse[]
+    }
+
+    const { status: genresStatus, data: genres } = useFetchApi<RawGenresApi>({ queryKey: "genres", url: "/genres.json" });
+    // const genresList = useAppSelector(selectGenresList);
+    // @ts-ignore
+    const extractedGenres = genresIds.map(id => genres?.genres?.find(genre => id === genre.id));
     const isGenresIdsEmpty = genresIds.length > 0;
 
     return (
