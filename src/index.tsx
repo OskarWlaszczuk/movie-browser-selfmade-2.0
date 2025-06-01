3d4bfc2db2b06d0d99a7a362ab481df50f08cf69
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-06-01 17:37:22 +0200

    Wrap application in QueryClientProvider for React Query integration

--- src/index.tsx
+++ src/index.tsx
@@ -7,16 +7,20 @@ import { ThemeProvider } from 'styled-components';
 import { theme } from './core/theme';
 import { Provider } from 'react-redux';
 import { store } from './core/store';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 const root = ReactDOM.createRoot(document.getElementById('root')!);
+const queryClient = new QueryClient();
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <ThemeProvider theme={theme}>
-        <GlobalStyles />
-        <App />
-      </ThemeProvider>
-    </Provider>
+    <QueryClientProvider client={queryClient}>
+      <Provider store={store}>
+        <ThemeProvider theme={theme}>
+          <GlobalStyles />
+          <App />
+        </ThemeProvider>
+      </Provider>
+    </QueryClientProvider>
   </React.StrictMode>
 );
 
