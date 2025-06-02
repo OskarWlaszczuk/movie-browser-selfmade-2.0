2db2706d3faf94007f58c1faa5d5c67af682c599
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-06-02 15:58:43 +0200

    Added key attribude for each TilesListSection rendered in iteration. Removed generic type ListItem and replaced it with SectionDataUnion[] type

--- src/features/DetailsPage/index.tsx
+++ src/features/DetailsPage/index.tsx
@@ -1,33 +1,26 @@
-import { JSX } from "react";
 import { Movie } from "../../common/aliases/interfaces/Movie"
 import { Person } from "../../common/aliases/interfaces/Person"
 import { TilesListSection } from "../../common/components/TilesListSection";
 import { FetchStatus } from "../../common/aliases/types/FetchStatus";
 import { Main } from "../../common/components/Main";
 import { useCombinedFetchStatus } from "../../common/hooks/useCombinedFetchStatus";
+import { nanoid } from "@reduxjs/toolkit";
+import { CastMember, CrewMember } from "../../common/functions/renderPersonItem";
+import { TilesSectionData } from "../../common/aliases/interfaces/TilesSectionData";
 
-interface TitleData {
-    text: string;
-    isPageTitle: boolean;
-}
-
-type RenderListItem<ListItem> = (item: ListItem) => JSX.Element;
-
-interface SectionData<ListItem> {
-    list: ListItem[];
-    titleData: TitleData;
-    renderListItem: RenderListItem<ListItem>
-}
-
-interface DetailsPageProps<ListItem> {
+type SectionDataUnion =
+    | TilesSectionData<CrewMember>
+    | TilesSectionData<CastMember>
+    | TilesSectionData<Movie>
+    | TilesSectionData<Person>;
+;
+interface DetailsPageProps {
     details?: Movie | Person;
-    sectionsData: SectionData<ListItem>[];
+    sectionsData: SectionDataUnion[];
     fetchStatuses: FetchStatus[];
 }
 
-export const DetailsPage = <ListItem extends Movie | Person>(
-    { details, sectionsData, fetchStatuses }: DetailsPageProps<ListItem>
-) => {
+export const DetailsPage = ({ details, sectionsData, fetchStatuses }: DetailsPageProps) => {
 
     const combinedFetchStatus = useCombinedFetchStatus(fetchStatuses);
 
@@ -36,10 +29,10 @@ export const DetailsPage = <ListItem extends Movie | Person>(
             <Main
                 content={
                     <>
-                    
+
                         {
                             sectionsData.map(sectionData => (
-                                <TilesListSection {...sectionData} />
+                                <TilesListSection key={nanoid()} {...sectionData} />
                             ))
                         }
                     </>
