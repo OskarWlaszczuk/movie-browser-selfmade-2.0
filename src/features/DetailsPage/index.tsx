cb000363eb0bd5112da2e65ef9eabfc1477d2b4c
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-06-02 16:29:54 +0200

    Removed TilesSectionData<Person> type from SectionDataUnion

--- src/features/DetailsPage/index.tsx
+++ src/features/DetailsPage/index.tsx
@@ -1,18 +1,15 @@
-import { Movie } from "../../common/aliases/interfaces/Movie"
-import { Person } from "../../common/aliases/interfaces/Person"
 import { TilesListSection } from "../../common/components/TilesListSection";
 import { FetchStatus } from "../../common/aliases/types/FetchStatus";
 import { Main } from "../../common/components/Main";
 import { useCombinedFetchStatus } from "../../common/hooks/useCombinedFetchStatus";
 import { nanoid } from "@reduxjs/toolkit";
-import { CastMember, CrewMember } from "../../common/functions/renderPersonItem";
 import { TilesSectionData } from "../../common/aliases/interfaces/TilesSectionData";
+import { CastMember, CrewMember, Movie, Person } from "../../common/aliases/interfaces/Entities";
 
 type SectionDataUnion =
     | TilesSectionData<CrewMember>
     | TilesSectionData<CastMember>
     | TilesSectionData<Movie>
-    | TilesSectionData<Person>;
 ;
 interface DetailsPageProps {
     details?: Movie | Person;
