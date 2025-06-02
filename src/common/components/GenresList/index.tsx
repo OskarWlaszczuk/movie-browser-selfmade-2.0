627ac343faac261d513329f669b23bc0b597374f
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-06-02 16:27:13 +0200

    Removed genres selector import and useAppSelect

--- src/common/components/GenresList/index.tsx
+++ src/common/components/GenresList/index.tsx
@@ -1,6 +1,4 @@
 import { GenreResponse, GenresIds } from "../../aliases/types/genre.types";
-// import { selectGenresList } from "../../../genresSlice";
-// import { useAppSelector } from "../../../reduxTypedHooks";
 import { Genre, StyledGenres } from "./styled";
 import { useFetchApi } from "../../hooks/useFetchApi";
 
@@ -14,8 +12,7 @@ export const GenresList = ({ genresIds }: GenresListProps) => {
     }
 
     const { status: genresStatus, data: genres } = useFetchApi<RawGenresApi>({ queryKey: "genres", url: "/genres.json" });
-    // const genresList = useAppSelector(selectGenresList);
-    // @ts-ignore
+
     const extractedGenres = genresIds.map(id => genres?.genres?.find(genre => id === genre.id));
     const isGenresIdsEmpty = genresIds.length > 0;
 
@@ -25,7 +22,7 @@ export const GenresList = ({ genresIds }: GenresListProps) => {
                 isGenresIdsEmpty && (
                     <StyledGenres>
                         {
-                            extractedGenres.map((genre, index) => (
+                            extractedGenres?.map((genre, index) => (
                                 <Genre key={index}>{genre?.name}</Genre>
                             ))
                         }
