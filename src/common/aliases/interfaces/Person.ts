8308a0d7085e1da436337ca4a575ebe86050aa3b
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-06-02 16:26:31 +0200

    Moved Movie, Person and TileEntity types to one shared file

--- src/common/aliases/interfaces/Person.ts
+++
@@ -1,8 +0,0 @@
-export interface Person {
-    id: number;
-    name: string;
-    profile_path: string;
-    biography: string;
-    birthday: string;
-    place_of_birth: string;
-}
--- src/common/aliases/types/TileEntity.ts
+++
@@ -1,5 +0,0 @@
-import { CastMember, CrewMember } from "../../functions/renderPersonItem";
-import { Movie } from "../interfaces/Movie";
-import { Person } from "../interfaces/Person";
-
-export type TileEntity = Movie | Person | CastMember | CrewMember;
