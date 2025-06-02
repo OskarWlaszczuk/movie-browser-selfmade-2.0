69d150319518f985f55f83eeeca88ce62ffd318a
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-06-02 15:55:52 +0200

    Update Movie interface to use OrNull for backdrop_path and poster_path properties

--- src/common/aliases/interfaces/Movie.ts
+++ src/common/aliases/interfaces/Movie.ts
@@ -1,11 +1,12 @@
 import { GenresIds } from "../types/genre.types";
+import { OrNull } from "../types/OrNull";
 
 export interface Movie {
-    backdrop_path: string;
+    backdrop_path: OrNull<string>;
     genre_ids: GenresIds;
     id: number;
     overview: string;
-    poster_path: string;
+    poster_path: OrNull<string>;
     release_date: string;
     title: string;
     vote_average: number;
