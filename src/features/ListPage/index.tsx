fa7212ccccfb814df17f2d919992073fe1a92f83
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-06-02 15:53:31 +0200

    Passed Movie[] | Person[] alias type to OrUndefined as generic type as the list might be undefined.

--- src/features/ListPage/index.tsx
+++ src/features/ListPage/index.tsx
@@ -1,29 +1,28 @@
 import { Movie } from "../../common/aliases/interfaces/Movie";
 import { Person } from "../../common/aliases/interfaces/Person";
 import { FetchStatus } from "../../common/aliases/types/FetchStatus";
+import { OrUndefined } from "../../common/aliases/types/OrUndefined";
 import { Main } from "../../common/components/Main";
-import { renderMovieItem } from "../../common/functions/renderMovieItem";
-import { renderPersonItem } from "../../common/functions/renderPersonItem";
 import { TilesListSection } from "../../common/components/TilesListSection";
 import { useCombinedFetchStatus } from "../../common/hooks/useCombinedFetchStatus";
 
 interface ListPageProps {
     title: string;
-    list: Movie[] | Person[];
+    list: OrUndefined<Movie[] | Person[]>;
     fetchStatuses: FetchStatus[]
 }
 
 export const ListPage = ({ title, list, fetchStatuses }: ListPageProps) => {
     const isMoviesList = (list: Movie[] | Person[]): list is Movie[] => {
-        return list.length > 0 && "title" in list[0];
+        return list?.length > 0 && "title" in list?.[0];
     };
 
     const titleData = { isPageTitle: true, text: title };
 
     const tilesListSectionElement = (
-        isMoviesList(list) ?
-            <TilesListSection list={list} titleData={titleData} renderListItem={renderMovieItem} /> :
-            <TilesListSection list={list} titleData={titleData} renderListItem={renderPersonItem} />
+        isMoviesList(list!) ?
+            <TilesListSection list={list} titleData={titleData} /> :
+            <TilesListSection list={list} titleData={titleData} />
 
     );
 
