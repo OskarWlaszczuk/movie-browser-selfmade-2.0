da9e16fe760d6f0f002b49c2148017ac0e73b631
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-06-02 16:25:48 +0200

    removed all sagas and slices, they were replaced by useFetchApi hook

--- src/popularMoviesSlice.ts
+++
@@ -1,8 +0,0 @@
-import { createPopularListSlice } from "./common/functions/createPopularListSlice";
-import { Movie } from "./common/aliases/interfaces/Movie";
-// @ts-ignore
-export const popularMoviesSlice = createPopularListSlice({ stateName: "popularMovies" });
-// @ts-ignore
-export const popularMoviesReducer = popularMoviesSlice.reducer;
-
-export const { actions: popularMoviesActions, selectors: popularMoviesSelectors } = popularMoviesSlice;
--- src/popularPeopleSlice.ts
+++
@@ -1,8 +0,0 @@
-import { createPopularListSlice } from "./common/functions/createPopularListSlice";
-import { Person } from "./common/aliases/interfaces/Person";
-// @ts-ignore
-export const popularPeopleSlice = createPopularListSlice<Person>({ stateName: "popularPeople" });
-// @ts-ignore
-export const popularPeopleReducer = popularPeopleSlice.reducer;
-
-export const { actions: popularPeopleActions, selectors: popularPeopleSelectors } = popularPeopleSlice;
