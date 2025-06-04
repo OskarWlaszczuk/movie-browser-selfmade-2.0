b30fcadfb4baa18bded4a4dd0a903e05ed593ee3
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-06-04 15:36:48 +0200

    Refactor Person component to replace useFetchApi with useFetchDetails for fetching person details

--- src/features/DetailsPage/Person/index.tsx
+++ src/features/DetailsPage/Person/index.tsx
@@ -1,27 +1,22 @@
 import { useParams } from "react-router-dom";
 import { DetailsPage } from "..";
-import { useFetchApi } from "../../../common/hooks/useFetchApi";
-import { apiUrls } from "../../../common/constants/pictureConfigs";
 import { useCredits } from "../../../common/hooks/useCredits";
 import { PersonCreditsApiResponse } from "../../../common/aliases/interfaces/creditsApiResponses.types";
 import { PersonDetails } from "../../../common/aliases/interfaces/person.types";
+import { useFetchDetails } from "../../../common/hooks/useFetchDetails";
 
 export const Person = () => {
     const { personId } = useParams();
 
     const { creditsSectionsData, creditsStatus } = useCredits<PersonCreditsApiResponse>(personId!, `person/${personId}/credits`);
+    const { details, detailsStatus } = useFetchDetails<PersonDetails>(personId!, `person/${personId}`);
 
-    const { status: personStatus, data: person } = useFetchApi<PersonDetails>({
-        queryKey: "movieDetails",
-        url: `${apiUrls.base}/person/${personId}`,
-        urlDependencies: [personId!]
-    });
 
     return (
         <DetailsPage
-            details={person!}
+            details={details!}
             sectionsData={creditsSectionsData}
-            fetchStatuses={[personStatus, creditsStatus]}
+            fetchStatuses={[detailsStatus, creditsStatus]}
         />
     );
 }
