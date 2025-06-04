014dd9098dc9c89c407a0ae0079a905544bfd594
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-06-04 16:35:19 +0200

    Moved useFetchPopularList file to local  'hooks' folder in ListPage feature because this hook is used only inside of ListPage components

--- src/features/ListPage/hooks/useFetchPopularList.ts
+++ src/features/ListPage/hooks/useFetchPopularList.ts
@@ -1,5 +1,5 @@
-import { PopularMovieApi, PopularPeopleApi } from "../aliases/interfaces/TMDBRList";
-import { useFetchApi } from "./useFetchApi";
+import { PopularMovieApi, PopularPeopleApi } from "../../../common/aliases/interfaces/popularListApi.types";
+import { useFetchApi } from "../../../common/hooks/useFetchApi";
 
 export const useFetchPopularList = <PopularListApiResponse extends PopularMovieApi | PopularPeopleApi>(url: string) => {
     const {
