7842955ea27844eb21a1dc183dd2a5488315504d
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-06-04 16:19:41 +0200

    Integrate useFetchGenres into Movies component for genre status handling

--- src/features/ListPage/Movies/index.tsx
+++ src/features/ListPage/Movies/index.tsx
@@ -1,15 +1,17 @@
 import { ListPage } from "../index";
 import { PopularMovieApi } from "../../../common/aliases/interfaces/TMDBRList";
 import { useFetchPopularList } from "../../../common/hooks/useFetchPopularList";
+import { useFetchGenres } from "../../../common/hooks/useFetchGenres";
 
 export const Movies = () => {
     const { popularList, popularListStatus } = useFetchPopularList<PopularMovieApi>("/popularMovies.json");
+    const genresStatus = useFetchGenres();
 
     return (
         <ListPage
             title="Popular movies"
             list={popularList?.results}
-            fetchStatuses={[popularListStatus]}
+            fetchStatuses={[popularListStatus, genresStatus]}
         />
     );
 };
