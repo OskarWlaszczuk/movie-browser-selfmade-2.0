f2af4eb6ae1b71007b3b12cdabd4b841e8b06afd
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-06-08 18:20:33 +0200

    Update ListPage component to use listData prop and include currentPage in Pagination

--- src/features/ListPage/components/index.tsx
+++ src/features/ListPage/components/index.tsx
@@ -3,14 +3,20 @@ import { TilesListSection } from "../../../common/components/TilesListSection";
 import { Main } from "../../../common/components/Main";
 import { useCombinedFetchStatus } from "../../../common/hooks/useCombinedFetchStatus";
 import { ListPageProps } from "../types/ListPageProps";
+import { Pagination } from "./Pagination";
 
-export const ListPage = <ListType extends PeopleOrMovies>({ title, list, fetchStatuses }: ListPageProps<ListType>) => {
+export const ListPage = <ListType extends PeopleOrMovies>({ title, listData, fetchStatuses }: ListPageProps<ListType>) => {
     const combinedFetchStatus = useCombinedFetchStatus(fetchStatuses);
 
     return (
         <>
             <Main
-                content={<TilesListSection list={list} titleData={{ isPageTitle: true, text: title }} />}
+                content={
+                    <>
+                        <TilesListSection list={listData?.results} titleData={{ isPageTitle: true, text: title }} />
+                        <Pagination totaPages={listData?.total_pages} />
+                    </>
+                }
                 combinedFetchStatus={combinedFetchStatus}
             />
         </>
