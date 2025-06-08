2ab7efe104713eade9639fb7a39442f66161719b
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-06-08 18:23:15 +0200

    Update ListPageProps to rename list to listData and adjust type to use ListApiResponse

--- src/features/ListPage/types/ListPageProps.ts
+++ src/features/ListPage/types/ListPageProps.ts
@@ -1,9 +1,10 @@
 import { FetchStatus } from "../../../common/aliases/types/FetchStatus";
 import { OrUndefined } from "../../../common/aliases/types/OrUndefined";
 import { PeopleOrMovies } from "../../../common/aliases/types/PeopleOrMovies";
+import { ListApiResponse } from "./listApi.types";
 
 export interface ListPageProps<ListType extends PeopleOrMovies> {
     title: string;
-    list: OrUndefined<ListType>;
+    listData: OrUndefined<ListApiResponse<ListType>>;
     fetchStatuses: FetchStatus[]
 }
