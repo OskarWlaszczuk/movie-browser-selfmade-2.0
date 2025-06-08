320c2a4c97d7c1d434377627933edfba2d0e628f
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-06-08 11:45:19 +0200

    Refactor People component to use updated hooks and props for rendering

--- src/features/ListPage/components/People/index.tsx
+++ src/features/ListPage/components/People/index.tsx
@@ -1,15 +1,18 @@
 import { ListPage } from "..";
 import { useFetchPopularList } from "../../hooks/useFetchPopularList";
-import { PopularPeopleApi } from "../../types/popularListApi.types";
+import { useFetchResultsProps } from "../../hooks/useFetchResultsProps";
+import { useSelectListPageProps } from "../../hooks/useSelectListPageProps";
+import { PeopleListApi } from "../../types/listApi.types";
 
 export const People = () => {
-    const { popularList, popularListStatus } = useFetchPopularList<PopularPeopleApi>("/popularPeople.json");
+    const resultsProps = useFetchResultsProps<PeopleListApi>({ searchType: "person" });
+    const popularListProps = useFetchPopularList<PeopleListApi>("/popularPeople.json");
+
+    const selectedListPageProps = useSelectListPageProps({ resultsProps, popularListProps });
 
     return (
-        <ListPage
-            title="Popular people"
-            list={popularList?.results}
-            fetchStatuses={[popularListStatus]}
-        />
+        <>
+            <ListPage {...selectedListPageProps} />
+        </>
     );
 };
