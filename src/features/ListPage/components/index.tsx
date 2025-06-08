c0884c8195fd691514fb209fd4ae5eab4e39968b
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-06-08 11:31:25 +0200

    Refactor ListPage component to simplify rendering logic and remove unused types

--- src/features/ListPage/components/index.tsx
+++ src/features/ListPage/components/index.tsx
@@ -1,39 +1,16 @@
-import { FetchStatus } from "../../../common/aliases/types/FetchStatus";
-import { OrUndefined } from "../../../common/aliases/types/OrUndefined";
 import { PeopleOrMovies } from "../../../common/aliases/types/PeopleOrMovies";
-import { MovieItem } from "../../../common/aliases/interfaces/movie.types";
 import { TilesListSection } from "../../../common/components/TilesListSection";
 import { Main } from "../../../common/components/Main";
 import { useCombinedFetchStatus } from "../../../common/hooks/useCombinedFetchStatus";
+import { ListPageProps } from "../types/ListPageProps";
 
-
-interface ListPageProps {
-    title: string;
-    list: OrUndefined<PeopleOrMovies>;
-    fetchStatuses: FetchStatus[]
-}
-
-export const ListPage = ({ title, list, fetchStatuses }: ListPageProps) => {
-    //inny type guard
-    const isMoviesList = (list: PeopleOrMovies): list is MovieItem[] => {
-        return list?.length > 0 && "title" in list?.[0];
-    };
-
-    const titleData = { isPageTitle: true, text: title };
-
-    const tilesListSectionElement = (
-        isMoviesList(list!) ?
-            <TilesListSection list={list} titleData={titleData} /> :
-            <TilesListSection list={list} titleData={titleData} />
-
-    );
-
+export const ListPage = <ListType extends PeopleOrMovies>({ title, list, fetchStatuses }: ListPageProps<ListType>) => {
     const combinedFetchStatus = useCombinedFetchStatus(fetchStatuses);
 
     return (
         <>
             <Main
-                content={<>{tilesListSectionElement}</>}
+                content={<TilesListSection list={list} titleData={{ isPageTitle: true, text: title }} />}
                 combinedFetchStatus={combinedFetchStatus}
             />
         </>
