7bdc821deab1723a5336c09fd795471b0b4c7ac8
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-06-08 18:23:02 +0200

    Refactor ListApiResponse to use a more generic ResultsType and update type definitions for PeopleListApi and MoviesListApi

--- src/features/ListPage/types/listApi.types.ts
+++ src/features/ListPage/types/listApi.types.ts
@@ -1,14 +1,15 @@
 import { MovieItem } from "../../../common/aliases/interfaces/movie.types";
 import { PersonItem } from "../../../common/aliases/interfaces/person.types";
+import { PeopleOrMovies } from "../../../common/aliases/types/PeopleOrMovies";
 
-export interface ListApiResponse<ListItemType extends MovieItem | PersonItem> {
+export interface ListApiResponse<ResultsType extends PeopleOrMovies> {
     page: number;
-    results: ListItemType[];
+    results: ResultsType;
     total_pages: number;
     total_results: number;
 }
 
-export type PeopleListApi = ListApiResponse<PersonItem>;
-export type MoviesListApi = ListApiResponse<MovieItem>;
+export type PeopleListApi = ListApiResponse<PersonItem[]>;
+export type MoviesListApi = ListApiResponse<MovieItem[]>;
 
 export type ListApiUnion = PeopleListApi | MoviesListApi;
