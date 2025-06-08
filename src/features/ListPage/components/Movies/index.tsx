6bd0086b9b040880044e2925ab6b04f82df83815
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-06-08 09:42:17 +0200

    Fetches searching movie results using useFetchResults hook. Rendering both popular movies list and searching results using one ListPage component by dynamically selects its props based on truthfulness of search.

--- src/features/ListPage/components/Movies/index.tsx
+++ src/features/ListPage/components/Movies/index.tsx
@@ -1,17 +1,49 @@
 import { useFetchPopularList } from "../../hooks/useFetchPopularList";
 import { useFetchGenres } from "../../../../common/hooks/useFetchGenres";
 import { ListPage } from "..";
-import { PopularMovieApi } from "../../types/popularListApi.types";
+import { ListApiUnion, MoviesListApi } from "../../types/listApi.types";
+import { useQueryParameter } from "../../../../common/hooks/useQueryParameter";
+import { useFetchApi } from "../../../../common/hooks/useFetchApi";
+import { apiUrls } from "../../../../common/constants/pictureConfigs";
+import { OrUndefined } from "../../../../common/aliases/types/OrUndefined";
+import { PeopleOrMovies } from "../../../../common/aliases/types/PeopleOrMovies";
+import { FetchStatus } from "../../../../common/aliases/types/FetchStatus";
+import { useFetchResults } from "../../hooks/useFetchResults";
 
 export const Movies = () => {
-    const { popularList, popularListStatus } = useFetchPopularList<PopularMovieApi>("/popularMovies.json");
+    const { search, pageNumber } = useQueryParameter();
+
+    const { popularList, popularListStatus } = useFetchPopularList<MoviesListApi>("/popularMovies.json");
     const genresStatus = useFetchGenres();
 
+    const { resultsDataStatus, resultsData } = useFetchResults({ searchType: "movie" });
+    interface ListPageProps {
+        title: string;
+        list: OrUndefined<PeopleOrMovies>;
+        fetchStatuses: FetchStatus[]
+    }
+    const resultsList = resultsData?.results;
+
+    const getListPageProps = (): ListPageProps => (
+        !search ?
+            {
+                title: "Popular movies",
+                list: popularList?.results,
+                fetchStatuses: [popularListStatus, genresStatus]
+            } :
+            {
+                title: `Search for "${search}" (${resultsList?.length})`,
+                list: resultsList,
+                fetchStatuses: [resultsDataStatus]
+            }
+    );
+
+    const listPageProps = getListPageProps();
     return (
-        <ListPage
-            title="Popular movies"
-            list={popularList?.results}
-            fetchStatuses={[popularListStatus, genresStatus]}
-        />
+        <>
+            <ListPage
+                {...listPageProps}
+            />
+        </>
     );
 };
