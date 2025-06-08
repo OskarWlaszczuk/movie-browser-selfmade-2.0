e38eefa2b38b002a9b1ce82295702e6cbf2727cd
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-06-08 11:43:11 +0200

    Update getYear function to handle undefined release dates

--- src/common/functions/getYear.ts
+++ src/common/functions/getYear.ts
@@ -1,3 +1,4 @@
 import { MovieItem } from "../aliases/interfaces/movie.types";
+import { OrUndefined } from "../aliases/types/OrUndefined";
 
-export const getYear = (rawReleaseDate: MovieItem["release_date"]) => new Date(rawReleaseDate).getFullYear();
+export const getYear = (rawReleaseDate: OrUndefined<MovieItem["release_date"]>) => rawReleaseDate ? new Date(rawReleaseDate).getFullYear() : undefined;
