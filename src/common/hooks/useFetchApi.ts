e0ad27d5f0016af91f7fcce3f738b6c2b2403482
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-06-08 11:43:25 +0200

    Add fetchCondition parameter to useFetchApi hook for conditional fetching

--- src/common/hooks/useFetchApi.ts
+++ src/common/hooks/useFetchApi.ts
@@ -4,13 +4,14 @@ interface UseFetchApiInput {
     queryKey: string;
     url: string;
     urlDependencies?: (string | number)[];
+    fetchCondition?: boolean;
 }
 
-//Wyciąganie dynamicznej właściwości już wewnątrz hooka
-export const useFetchApi = <ResponseType,>({ queryKey, url, urlDependencies = [] }: UseFetchApiInput) => {
+export const useFetchApi = <ResponseType,>({ queryKey, url, urlDependencies = [], fetchCondition = true }: UseFetchApiInput) => {
     const { status, data } = useQuery({
         queryKey: [queryKey, ...urlDependencies],
         queryFn: () => fetchFromAPI<ResponseType>(url),
+        enabled: fetchCondition,
     });
 
     return { status, data };
