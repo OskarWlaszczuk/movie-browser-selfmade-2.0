7b2b4c06255f1eeeed56f9a62c13cb4d0637bc4b
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-06-20 14:23:48 +0200

    Refactor TilesListSection styled component to add type definition for props and improve grid layout logic

--- src/common/components/TilesListSection/styled.ts
+++ src/common/components/TilesListSection/styled.ts
@@ -1,7 +1,16 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
-export const TilesList = styled.div`
+interface TilesListProps {
+    $moreItems?: boolean;
+}
+
+export const TilesList = styled.div<TilesListProps>`
     display: grid;
     grid-gap: 24px;
-    grid-template-columns: repeat(auto-fit, minmax(270px, 1fr))
+    grid-template-columns: repeat(auto-fit, minmax(270px, 1fr));
+
+
+${({ $moreItems }) => $moreItems && css`
+    grid-template-columns: repeat(auto-fill, minmax(195px, 1fr));
+`};
 `;
