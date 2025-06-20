45271146a64e24f79e6f0abca7cb0ebe4dd02894
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-06-20 15:20:48 +0200

    simplefied creditsApiResponse.types folder name to credits.types

--- src/features/DetailsPage/types/credits.types.ts
+++ src/features/DetailsPage/types/credits.types.ts
@@ -1,12 +1,14 @@
 import { SimplefiedMovieItem } from "../../../common/aliases/interfaces/movie.types";
 import { CastMember, CrewMember } from "../../../common/aliases/interfaces/person.types";
 
-export interface MovieCreditsApiResponse {
+export interface MovieCredits {
     cast: CastMember[];
     crew: CrewMember[];
 }
 
-export interface PersonCreditsApiResponse {
+export interface PersonCredits {
     cast: SimplefiedMovieItem[];
     crew: SimplefiedMovieItem[];
 }
+
+export type CreditsType = MovieCredits | PersonCredits;
