4d6df36bda0bfe5c90550f5a6f38891083fde2a6
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-06-20 12:42:00 +0200

    Add TileEntityId type to extract id from TileEntity

--- src/common/aliases/interfaces/TileEntity.ts
+++ src/common/aliases/interfaces/TileEntity.ts
@@ -2,3 +2,5 @@ import { MovieItem } from "./movie.types";
 import { CastMember, CrewMember, PersonItem } from "./person.types";
 
 export type TileEntity = MovieItem | PersonItem | CastMember | CrewMember;
+
+export type TileEntityId = TileEntity["id"]
