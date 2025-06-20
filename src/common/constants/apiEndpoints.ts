823a6a5d6f32a3bc4258a66df1c7d24015089b24
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-06-20 13:39:47 +0200

    Refactor apiEndpoints to use consistent naming for movie and person functions for improved clarity

--- src/common/constants/apiEndpoints.ts
+++ src/common/constants/apiEndpoints.ts
@@ -1,16 +1,21 @@
 import { TileEntityId } from "../aliases/interfaces/TileEntity";
 
 const baseEntityEndpoints = {
-    movie: (movieId: TileEntityId) => `movie/${movieId}`,
-    person: (personId: TileEntityId) => `person/${personId}`,
+    getMovie: (movieId: TileEntityId) => `movie/${movieId}`,
+    getPerson: (personId: TileEntityId) => `person/${personId}`,
 };
 
 export const creditsEndpoints = {
-    getPersonCredits: (personId: TileEntityId) => `${baseEntityEndpoints.person(personId)}/credits`,
-    getMovieCredits: (movieId: TileEntityId) => `${baseEntityEndpoints.movie(movieId)}/credits`,
+    getPersonCredits: (personId: TileEntityId) => `${baseEntityEndpoints.getPerson(personId)}/credits`,
+    getMovieCredits: (movieId: TileEntityId) => `${baseEntityEndpoints.getMovie(movieId)}/credits`,
 };
 
 export const detailsEndpoints = {
-    getPersonDetails: (personId: TileEntityId) => `${baseEntityEndpoints.person(personId)}`,
-    getMovieDetails: (movieId: TileEntityId) => `${baseEntityEndpoints.movie(movieId)}`,
+    getPersonDetails: (personId: TileEntityId) => `${baseEntityEndpoints.getPerson(personId)}`,
+    getMovieDetails: (movieId: TileEntityId) => `${baseEntityEndpoints.getMovie(movieId)}`,
 };
+
+export const popularListsEndpoints = {
+    persons: "person/popular",
+    movies: "movie/popular",
+} as const;
