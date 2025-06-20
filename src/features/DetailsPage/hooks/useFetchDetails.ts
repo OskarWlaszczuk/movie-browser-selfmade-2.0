0b057eca7b516faeca0fe355be5a21a14157be6e
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-06-20 13:14:43 +0200

    Refactor useFetchDetails to define DetailedEntityItem type separately for improved readability

--- src/features/DetailsPage/hooks/useFetchDetails.ts
+++ src/features/DetailsPage/hooks/useFetchDetails.ts
@@ -4,7 +4,9 @@ import { TileEntityId } from "../../../common/aliases/interfaces/TileEntity";
 import { apiUrls } from "../../../common/constants/pictureConfigs";
 import { useFetchApi } from "../../../common/hooks/useFetchApi";
 
-export const useFetchDetails = <Details extends DetailedMovieItem | DetailedPersonItem>(entityId: TileEntityId, endpoint: string) => {
+type DetailedEntityItem = DetailedMovieItem | DetailedPersonItem;
+
+export const useFetchDetails = <Details extends DetailedEntityItem>(entityId: TileEntityId, endpoint: string) => {
     const { status: detailsStatus, data: details } = useFetchApi<Details>({
         queryKey: "details",
         url: `${apiUrls.base}/${endpoint}`,
