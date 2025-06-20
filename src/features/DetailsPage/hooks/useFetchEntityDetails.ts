942b57bac11463fae54015d7ffb103619c6ba983
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-06-20 15:20:18 +0200

    Renamed useFetchDetails to useFetchEntityDetails

--- src/features/DetailsPage/hooks/useFetchEntityDetails.ts
+++ src/features/DetailsPage/hooks/useFetchEntityDetails.ts
@@ -2,7 +2,7 @@ import { TileEntityId } from "../../../common/aliases/interfaces/TileEntity";
 import { DetailedEntityItem } from "../../../common/aliases/types/DetailedEntityItem";
 import { useFetchApi } from "../../../common/hooks/useFetchApi";
 
-export const useFetchDetails = (entityId: TileEntityId, endpoint: string) => {
+export const useFetchEntityDetails = (entityId: TileEntityId, endpoint: string) => {
     const { status: detailsStatus, data: details } = useFetchApi<DetailedEntityItem>({
         queryKey: "details",
         endpoint,
