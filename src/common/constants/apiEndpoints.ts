04e003beaa4756a2ab184a37ea5ea79493a3f29e
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-06-20 13:07:23 +0200

    Add detailsEndpoints for getting person and movie details endpoints required for fetching details

--- src/common/constants/apiEndpoints.ts
+++ src/common/constants/apiEndpoints.ts
@@ -4,3 +4,8 @@ export const creditsEndpoints = {
     getPersonCredits: (personId: TileEntityId) => `person/${personId}/credits`,
     getMovieCredits: (movieId: TileEntityId) => `movie/${movieId}/credits`,
 };
+
+export const detailsEndpoints = {
+    getPersonDetails: (personId: TileEntityId) => `person/${personId}`,
+    getMovieDetails: (movieId: TileEntityId) => `movie/${movieId}`,
+};
