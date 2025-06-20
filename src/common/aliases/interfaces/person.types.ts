9d23d86f49c58941e0b6b92956aaa4af75d96ca5
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-06-20 12:53:47 +0200

    Refactor person types to extend BasePersonData for CastMember, CrewMember, and PersonItem interfaces

--- src/common/aliases/interfaces/person.types.ts
+++ src/common/aliases/interfaces/person.types.ts
@@ -1,20 +1,19 @@
-export interface CastMember {
+interface BasePersonData {
     id: number;
     name: string;
+}
+
+export interface CastMember extends BasePersonData{
     profile_path: string;
     character: string;
 }
 
-export interface CrewMember {
-    id: number;
-    name: string;
+export interface CrewMember extends BasePersonData{
     profile_path: string;
     job: string;
 }
 
-export interface PersonItem {
-    id: number;
-    name: string;
+export interface PersonItem extends BasePersonData{
     profile_path: string;
 }
 
