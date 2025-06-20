79e1ea83d17e62756a4e6bd6bc2fdf48e2b9ac00
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-06-20 14:26:29 +0200

    Refactor usePopularListsProps hook to replace 'url' parameter with 'popularListEndpoint' for improved clarity and consistency

--- src/features/ListPage/hooks/usePopularListsProps.ts
+++ src/features/ListPage/hooks/usePopularListsProps.ts
@@ -4,17 +4,19 @@ import { useQueryParameter } from "../../../common/hooks/useQueryParameter";
 import { ListApiUnion } from "../types/listApi.types";
 import { ListPageProps } from "../types/ListPageProps";
 import { routes } from "../../../common/functions/routes";
+import { popularListsEndpoints } from "../../../common/constants/apiEndpoints";
 
 type PopularListProps<PopularListApiResponse extends ListApiUnion> = ListPageProps<PopularListApiResponse["results"]>;
+type PopularListsEndpoint = typeof popularListsEndpoints[keyof typeof popularListsEndpoints];
 
-export const usePopularListsProps = <PopularListApiResponse extends ListApiUnion>(url: string): PopularListProps<PopularListApiResponse> => {
+export const usePopularListsProps = <PopularListApiResponse extends ListApiUnion>(popularListEndpoint: PopularListsEndpoint): PopularListProps<PopularListApiResponse> => {
     const { search } = useQueryParameter();
     const { pathname } = useLocation()
 
     const {
         status: popularListStatus,
         data: popularList
-    } = useFetchApi<PopularListApiResponse>({ queryKey: "popularList", url, fetchCondition: !search });
+    } = useFetchApi<PopularListApiResponse>({ queryKey: "popularList", endpoint: popularListEndpoint, fetchCondition: !search });
 
     const popularListTitle = `Popular ${pathname === routes.movies() ? "movies" : "people"}`;
     const popularListProps: PopularListProps<PopularListApiResponse> = {
