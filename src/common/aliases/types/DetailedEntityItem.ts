c1b913800204fe5da7982eb9edc600951d79ef3e
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-06-20 15:19:14 +0200

    Add DetailedEntityItem type to unify movie and person item types

---
+++ src/common/aliases/types/DetailedEntityItem.ts
@@ -0,0 +1,4 @@
+import { DetailedMovieItem } from "../interfaces/movie.types";
+import { DetailedPersonItem } from "../interfaces/person.types";
+
+export type DetailedEntityItem = DetailedMovieItem | DetailedPersonItem;
