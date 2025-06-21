2ec988cf7989ebab531a9f67c1e9f91b27560d8f
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-06-21 17:59:10 +0200

    Call useQueryParameter hook and pass its return value further to hooks. If list is empty then displays ListPageNoResults component. Passed ListPageLoadingText component as extraLoaderContent prop to Main

--- src/features/ListPage/components/index.tsx
+++ src/features/ListPage/components/index.tsx
@@ -6,30 +6,41 @@ import { useListSectionProps } from "../hooks/useListSectionProps";
 import { popularListsEndpoints } from "../../../common/constants/apiEndpoints";
 import { useFetchGenres } from "../../../common/hooks/useFetchGenres";
 import { EntityType } from "../../../common/aliases/types/EntityType";
+import { useQueryParameter } from "../../../common/hooks/useQueryParameter";
+import { ListPageLoadingText } from "./ListPageLoadingText";
+import { ListPageNoResults } from "./ListPageNoResults";
 
 type PopularListsEndpoint = typeof popularListsEndpoints[keyof typeof popularListsEndpoints];
 
 interface ListPageProps {
-    entityType:EntityType;
+    entityType: EntityType;
     popularListEndpoint: PopularListsEndpoint
 }
 
 export const ListPage = ({ entityType, popularListEndpoint }: ListPageProps) => {
     const genresStatus = useFetchGenres();
+    const queryParams = useQueryParameter();
 
-    const selectedListSectionData = useListSectionProps({ entityType, popularListEndpoint });
+    const selectedListSectionData = useListSectionProps({ entityType, popularListEndpoint, queryParams });
     const combinedFetchStatus = useCombinedFetchStatus([...selectedListSectionData.fetchStatuses, genresStatus]);
+
+    const isListEmpty = !selectedListSectionData.listData?.results.length;
+
+    const content = (
+        isListEmpty ?
+            <ListPageNoResults search={queryParams.search} /> :
+            <>
+                <TilesListSection list={selectedListSectionData?.listData?.results} titleData={{ isPageTitle: true, text: selectedListSectionData.title }} />
+                <Pagination totaPages={selectedListSectionData?.listData?.total_pages} />
+            </>
+    );
 
     return (
         <>
             <Main
-                content={
-                    <>
-                        <TilesListSection list={selectedListSectionData?.listData?.results} titleData={{ isPageTitle: true, text: selectedListSectionData.title }} />
-                        <Pagination totaPages={selectedListSectionData?.listData?.total_pages} />
-                    </>
-                }
                 combinedFetchStatus={combinedFetchStatus}
+                extraLoaderContent={<ListPageLoadingText search={queryParams.search} />}
+                content={content}
             />
         </>
     );
