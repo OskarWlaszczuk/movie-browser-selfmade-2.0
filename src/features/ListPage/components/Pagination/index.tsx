0ddc58d604bd53f9eb0fd145736eefa80530fad0
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-06-21 18:39:57 +0200

    moved pagination buttons logic into usePaginationButtons hook

--- src/features/ListPage/components/Pagination/index.tsx
+++ src/features/ListPage/components/Pagination/index.tsx
@@ -1,59 +1,16 @@
-import { QUERY_PARAM_KEYS } from "../../../../common/constants/QUERY_PARAM_KEYS";
 import { useQueryParameter } from "../../../../common/hooks/useQueryParameter";
-import { useReplaceQueryParameter } from "../../../../common/hooks/useReplaceQueryParameter";
-import { PaginationButtonData } from "./types/PaginationButtonData";
 import { PaginationButtonGroup } from "./components/PaginationButtonGroup";
+import { usePaginationButtons } from "./hooks/usePaginationButtons";
 import { PageIndicator, PageNumber } from "./styled";
 
 export const Pagination = () => {
-    const replaceQueryParameter = useReplaceQueryParameter();
     const { pageNumber } = useQueryParameter();
-
-    const isFirstPage = pageNumber === 1;
-    const isLastPage = pageNumber === 500;
-
-    const paginationBackButtons: PaginationButtonData[] = [
-        {
-            clickHandler: () => replaceQueryParameter([{
-                key: QUERY_PARAM_KEYS.PAGE,
-                value: 1,
-            }]),
-            label: "First",
-            disabledCondition: isFirstPage,
-        },
-        {
-            clickHandler: () => replaceQueryParameter([{
-                key: QUERY_PARAM_KEYS.PAGE,
-                value: isFirstPage ? pageNumber : pageNumber! - 1
-            }]),
-            label: "Previous",
-            disabledCondition: isFirstPage,
-        },
-    ];
-
-    const paginationForwordButtons: PaginationButtonData[] = [
-        {
-            clickHandler: () => replaceQueryParameter([{
-                key: QUERY_PARAM_KEYS.PAGE,
-                value: isLastPage ? pageNumber : pageNumber! + 1
-            }]),
-            label: "Next",
-            disabledCondition: isLastPage,
-        },
-        {
-            clickHandler: () => replaceQueryParameter([{
-                key: QUERY_PARAM_KEYS.PAGE,
-                value: 500!,
-            }]),
-            label: "Last",
-            disabledCondition: isLastPage,
-        },
-    ];
+    const { paginationBackButtons, paginationForwordButtons, totalPages } = usePaginationButtons(pageNumber);
 
     return (
         <>
             <PaginationButtonGroup buttons={paginationBackButtons} />
-            <PageIndicator>Page <PageNumber>{pageNumber} </PageNumber>of<PageNumber> {500}</PageNumber></PageIndicator>
+            <PageIndicator>Page <PageNumber>{pageNumber} </PageNumber>of<PageNumber>{totalPages}</PageNumber></PageIndicator>
             <PaginationButtonGroup buttons={paginationForwordButtons} />
         </>
     );
