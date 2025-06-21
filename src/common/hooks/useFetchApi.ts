a0e6ef427725d7cd94637d7ce357f5d79bbaef38
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-06-21 17:53:52 +0200

    Add fetchDelayInSec option to useFetchApi for delayed API calls

--- src/common/hooks/useFetchApi.ts
+++ src/common/hooks/useFetchApi.ts
@@ -5,12 +5,25 @@ interface UseFetchApiInput {
     endpoint: string;
     urlDependencies?: (string | number)[];
     fetchCondition?: boolean;
+    fetchDelayInSec?: number;
 }
 
-export const useFetchApi = <ResponseType,>({ queryKey, endpoint, urlDependencies = [], fetchCondition = true }: UseFetchApiInput) => {
+export const useFetchApi = <ResponseType,>({
+    queryKey,
+    endpoint,
+    urlDependencies = [],
+    fetchCondition = true,
+    fetchDelayInSec = 0,
+}: UseFetchApiInput) => {
     const { status, data } = useQuery({
         queryKey: [queryKey, ...urlDependencies],
-        queryFn: () => fetchFromAPI<ResponseType>(endpoint),
+        queryFn: () =>
+            new Promise<ResponseType>((resolve) => {
+                setTimeout(async () => {
+                    const result = await fetchFromAPI<ResponseType>(endpoint);
+                    resolve(result);
+                }, fetchDelayInSec * 1000);
+            }),
         enabled: fetchCondition,
     });
 
