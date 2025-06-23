e87d7706b9820342369583fe461e77a7f6952678
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-06-23 20:43:23 +0200

    Refactor PaginationButton styles for consistency and improved theme integration

--- src/features/ListPage/components/Pagination/components/PaginationButtonGroup/styled.ts
+++ src/features/ListPage/components/Pagination/components/PaginationButtonGroup/styled.ts
@@ -1,7 +1,36 @@
 import styled from "styled-components";
+import { ReactComponent as NextPageIcon } from "../../NextPageIcon.svg"
+import { ReactComponent as PreviousPageIcon } from "../../PreviousPageIcon.svg"
 
 export const PaginationButton = styled.button`
+    font-size: ${({ theme }) => theme.fontSizes.s};
+    font-weight: ${({ theme }) => theme.fontWeights.regular};
+    background-color: ${({ theme }) => theme.colors.pattensBlue};
+    border: none;
+    border-radius:5px;
+    transition: 0.3s;
+    display: flex;
+    justify-content:center;
+    gap: 8px;
+
     &:disabled {
-        background-color: yellow;
+        background-color: ${({ theme }) => theme.colors.mystic};
+    }
+
+    &:hover {
+        filter: brightness(102%);
+    }
+
+`;
+
+export const StyledNextPageIcon = styled(NextPageIcon)`
+    path {
+        fill: ${({ theme }) => theme.colors.scienceBlue};
+    }
+`;
+
+export const StyledPreviousPageIcon = styled(PreviousPageIcon)`
+    path {
+        fill: ${({ theme }) => theme.colors.waterloo};
     }
 `;
