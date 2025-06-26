4f4a112e4ef2bd77d90801f86b2d637b1ef53f17
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-06-26 10:58:42 +0200

    Removed all props of useDetailsPageData, replaced them with one single parameter called endpointEntityType. Call useParams to get URL id param. Use endpointEntityType and id to declare endpoints for details and credits. Removed custom hook for fetching details and credits, moved useFetchApi directly inside useDetailsPageData body to simplify code. Moved creating credits sections outside of this hook. Now useDetailsPageData handles only fetching API data for DetailsPage component

--- src/features/DetailsPage/hooks/useDetailsPageData.ts
+++ src/features/DetailsPage/hooks/useDetailsPageData.ts
@@ -1,22 +1,46 @@
-import { TileEntityId } from "../../../common/aliases/interfaces/TileEntity";
-import { useCredits } from "./useCredits";
-import { useFetchEntityDetails } from "./useFetchEntityDetails";
+import { useParams } from "react-router-dom";
+import { entitiesDetailsEndpoints } from "../../../common/constants/apiEndpoints";
+import { useFetchApi } from "../../../common/hooks/useFetchApi";
+import { DetailedEntityItem } from "../../../common/aliases/types/DetailedEntityItem";
+import { CreditsType } from "../types/credits.types";
 
-interface UseDetailsPageDataProps {
-    id: TileEntityId;
-    detailsEndpoint: string;
-    creditsEndpoint: string;
-}
+type EntityDetailsEndpoints = typeof entitiesDetailsEndpoints[keyof typeof entitiesDetailsEndpoints];
 
-export const useDetailsPageData = ({ id, detailsEndpoint, creditsEndpoint }: UseDetailsPageDataProps) => {
-    const { details, detailsStatus } = useFetchEntityDetails(id, detailsEndpoint);
-    const { creditsSectionsData, creditsStatus } = useCredits(id, creditsEndpoint);
+export const useDetailsPageData = (endpointEntityType: EntityDetailsEndpoints) => {
+    const { id } = useParams();
 
-    const detailsPageDataStatuses = [detailsStatus, creditsStatus];
+    const detailsEndpoint = `${endpointEntityType}${id}`;
+    const creditsEndpoint = `${detailsEndpoint}/credits`;
+
+    const endpointDependecies = [id!];
+
+    const {
+        status: entityDetailsStatus,
+        data: entityDetails,
+        isPaused: isEntityDetailsPaused
+    } = useFetchApi<DetailedEntityItem>({
+        queryKey: "details",
+        endpoint: detailsEndpoint,
+        urlDependencies: endpointDependecies
+    });
+
+    const {
+        status: entityCreditsStatus,
+        data: entityCredits,
+        isPaused: isEntityCreditsPaused
+    } = useFetchApi<CreditsType>({
+        queryKey: "credits",
+        endpoint: creditsEndpoint,
+        urlDependencies: endpointDependecies,
+    });
+
+    const detailsPageDataStatuses = [entityDetailsStatus, entityCreditsStatus];
+    const detailsPausedFlags = [isEntityCreditsPaused, isEntityDetailsPaused];
 
     return {
-        details,
-        creditsSectionsData,
+        entityDetails,
+        entityCredits,
         detailsPageDataStatuses,
+        detailsPausedFlags,
     };
 };
