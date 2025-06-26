fe562298d3d22b0047cc72abdebec47c91bf166b
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-06-26 20:32:10 +0200

    Renamed useQueryParameter hook to useURLQueryParams

---
+++ src/common/hooks/useURLQueryParams.ts
@@ -0,0 +1,17 @@
+import { useLocation } from "react-router-dom";
+import { URL_QUERY_PARAM_KEYS } from "../constants/URL_QUERY_PARAM_KEYS";
+
+export const useURLQueryParams = () => {
+    const location = useLocation();
+    const searchParams = new URLSearchParams(location.search);
+    const URLQueryParamsKeys = ["page", "search"];
+    const obje = URLQueryParamsKeys.map((key) => ({
+        [key]: searchParams.get(key),
+    }))
+
+    console.log(obje)
+    const pageNumber = Number(searchParams.get(URL_QUERY_PARAM_KEYS.PAGE) || 1);
+    const search = searchParams.get(URL_QUERY_PARAM_KEYS.SEARCH) || "";
+
+    return { search, pageNumber };
+};
