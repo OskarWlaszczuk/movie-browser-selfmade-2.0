36818c0aad46df9c32cd05950666c3390230b796
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-06-26 20:35:26 +0200

    Removed export  for MovieCredits and PersonCredits, added 'Unnion' suffix to CreditsType

--- src/features/DetailsPage/types/credits.types.ts
+++ src/features/DetailsPage/types/credits.types.ts
@@ -1,14 +1,14 @@
 import { SimplefiedMovieItem } from "../../../common/aliases/interfaces/movie.types";
 import { CastMember, CrewMember } from "../../../common/aliases/interfaces/person.types";
 
-export interface MovieCredits {
+interface MovieCredits {
     cast: CastMember[];
     crew: CrewMember[];
 }
 
-export interface PersonCredits {
+interface PersonCredits {
     cast: SimplefiedMovieItem[];
     crew: SimplefiedMovieItem[];
 }
 
-export type CreditsType = MovieCredits | PersonCredits;
+export type CreditsTypeUnion = MovieCredits | PersonCredits;
