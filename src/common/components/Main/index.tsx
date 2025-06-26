ca0000e6c02e0c43e6a26bb5adf47c46ffc3eace
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-06-26 20:26:28 +0200

    Moved switch statemant into selectContentByStatus helper function. Removed MainSection component repetitions, now it is use one in the component. Call selectContentByStatus function inside MainContent element

--- src/common/components/Main/index.tsx
+++ src/common/components/Main/index.tsx
@@ -2,47 +2,46 @@ import { ReactNode } from "react";
 import { FetchStatus } from "../../aliases/types/FetchStatus";
 import { FETCH_STATUSES } from "../../constants/FETCH_STATUSES";
 import { MainContent, MainSection } from "./styled";
-import { Loader } from "../Loader";
 import { ErrorMessage } from "../ErrorMessage";
+import { Loader } from "../Loader";
 
 interface MainProps {
-    content: ReactNode;
-    extraLoaderContent?: ReactNode;
-    combinedFetchStatus: FetchStatus;
-    bannerContent?: ReactNode;
     errorMessage: string;
+    content: ReactNode;
+    combinedFetchStatus: FetchStatus;
+    extraLoaderContent?: ReactNode;
+    bannerContent?: ReactNode;
 };
 
 export const Main = ({ content, bannerContent, combinedFetchStatus, extraLoaderContent, errorMessage }: MainProps) => {
-    //Ustawić poprawne elementy zwracane w razie ładowania/błędu/initial
-    switch (combinedFetchStatus) {
-        case FETCH_STATUSES.SUCCESS:
-            return (
-                <MainSection>
-                    {bannerContent || <></>}
-                    <MainContent>
-                        {content}
-                    </MainContent>
-                </MainSection>
-            );
 
-        case FETCH_STATUSES.PENDING:
-            return (
-                <MainSection>
-                    <MainContent>
+    const selectContentByStatus = () => {
+        switch (combinedFetchStatus) {
+            case FETCH_STATUSES.SUCCESS:
+                return content
+
+            case FETCH_STATUSES.PENDING:
+                return (
+                    <>
                         {extraLoaderContent}
                         <Loader />
-                    </MainContent>
-                </MainSection>
-            );
+                    </>
+                );
 
-        case FETCH_STATUSES.ERROR:
-            return <ErrorMessage message={errorMessage} />;
+            case FETCH_STATUSES.ERROR:
+                return <ErrorMessage message={errorMessage} />;
 
-        case FETCH_STATUSES.PAUSED:
-            return <ErrorMessage message="Please check your network connection and try again" />;
-
-        default:
-            return <>Initial</>;
+            case FETCH_STATUSES.PAUSED:
+                return <ErrorMessage message="Please check your network connection and try again" />;
+        };
     };
+
+    return (
+        <MainSection>
+            {bannerContent || <></>}
+            <MainContent>
+                {selectContentByStatus()}
+            </MainContent>
+        </MainSection>
+    );
 };
