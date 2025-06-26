bdbd250cf336fa24aecc19ee07c437573f712369
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-06-26 11:07:44 +0200

    Use updated and simplefied version of EntityDetails in Person and Movie components

--- src/features/DetailsPage/components/Movie/index.tsx
+++ src/features/DetailsPage/components/Movie/index.tsx
@@ -1,16 +1,5 @@
-import { useParams } from "react-router-dom"
-import { DetailsPage } from "..";
-import { creditsEndpoints, detailsEndpoints } from "../../../../common/constants/apiEndpoints";
+import { EntityDetails } from "..";
 
-export const Movie = () => {
-    const { movieId } = useParams();
-    const numericMovieId = Number(movieId);
-
-    return (
-        <DetailsPage
-            id={numericMovieId}
-            creditsEndpoint={creditsEndpoints.getMovieCredits(numericMovieId)}
-            detailsEndpoint={detailsEndpoints.getMovieDetails(numericMovieId)}
-        />
-    );
-};
+export const Movie = () => (
+    <EntityDetails endpointEntityType="movie/" />
+);
--- src/features/DetailsPage/components/Person/index.tsx
+++ src/features/DetailsPage/components/Person/index.tsx
@@ -1,16 +1,5 @@
-import { useParams } from "react-router-dom";
-import { DetailsPage } from "..";
-import { creditsEndpoints, detailsEndpoints } from "../../../../common/constants/apiEndpoints";
+import { EntityDetails } from "..";
 
-export const Person = () => {
-    const { personId } = useParams();
-    const numericPersonId = Number(personId);
-
-    return (
-        <DetailsPage
-            id={numericPersonId}
-            creditsEndpoint={creditsEndpoints.getPersonCredits(numericPersonId)}
-            detailsEndpoint={detailsEndpoints.getPersonDetails(numericPersonId)}
-        />
-    );
-};
+export const Person = () => (
+    <EntityDetails endpointEntityType="person/" />
+);
