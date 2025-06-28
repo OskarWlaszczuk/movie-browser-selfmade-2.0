d2f13a21d7570a15bfcb892920cde497d7bd5296
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-06-28 19:39:33 +0200

    Add entity types constants for singular and plural forms

---
+++ src/common/constants/entityTypes.ts
@@ -0,0 +1,9 @@
+export const entitiesSingularTypes = {
+    MOVIE: "movie",
+    PERSON: "person",
+} as const;
+
+export const entitiesPluralTypes = {
+    MOVIES: "movies",
+    PEOPLE: "people",
+} as const; 
