a5ed1f30ce31dc7115e6abbb312eb312e57589d8
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-06-28 19:37:36 +0200

    Renamed QueryParams type to more specific name called UrlQueryKeyValuePair

--- src/common/aliases/interfaces/UrlQueryKeyValuePair.ts
+++ src/common/aliases/interfaces/UrlQueryKeyValuePair.ts
@@ -1,6 +1,6 @@
 import { URL_QUERY_PARAM_KEYS } from "../../constants/URL_QUERY_PARAM_KEYS";
 
-export interface QueryParam {
+export interface UrlQueryKeyValuePair {
     key: typeof URL_QUERY_PARAM_KEYS[keyof typeof URL_QUERY_PARAM_KEYS];
     value: string | number;
 }
