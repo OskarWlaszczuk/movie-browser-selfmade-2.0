0d18985df490a238c7212417212590e17569bfd3
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-06-30 12:27:55 +0200

    Moved profile_path prop to BasePersonData as it's used in every person type. Extended BasePersonData to include SharedTileEntityData type so removed id prop (as SharedTileEntityData already contains it) Changed SimplefiedPersonItem to type as it's no longer contains other props than these from BasePersonData. Passed birthday and place_of_birth props types to OrNull as generic arguments because these data may not be available in server response.

--- src/common/aliases/interfaces/person.types.ts
+++ src/common/aliases/interfaces/person.types.ts
@@ -1,24 +1,22 @@
-interface BasePersonData {
-    id: number;
+import { OrNull } from "../types/OrNull";
+import { SharedTileEntityData } from "./SharedTileEntityData";
+
+interface BasePersonData extends SharedTileEntityData {
     name: string;
+    profile_path: OrNull<string>;
 }
 
-export interface CastMember extends BasePersonData{
-    profile_path: string;
+export interface CastMember extends BasePersonData {
     character: string;
 }
 
-export interface CrewMember extends BasePersonData{
-    profile_path: string;
+export interface CrewMember extends BasePersonData {
     job: string;
 }
 
-export interface SimplefiedPersonItem extends BasePersonData{
-    profile_path: string;
-}
-
+export type SimplefiedPersonItem = BasePersonData;
 export interface DetailedPersonItem extends SimplefiedPersonItem {
     biography: string;
-    birthday: string;
-    place_of_birth: string;
+    birthday: OrNull<string>;
+    place_of_birth: OrNull<string>;
 }
