12ecd30e8f970e2626d234710ea4a5a614b57c2d
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-07-01 11:04:01 +0200

    Replaced renderVerticalTile function call in map iteration with VerticalTile component

--- src/common/components/TilesListSection/index.tsx
+++ src/common/components/TilesListSection/index.tsx
@@ -1,17 +1,11 @@
 import { TilesList } from "./styled";
 import { SectionHeader } from "../SectionHeader";
-import { renderVerticalTile } from "../../functions/renderVerticalTile";
-import { OrUndefined } from "../../aliases/types/OrUndefined";
+import { VerticalTile } from "./VerticalTile";
 import { TileEntity } from "../../aliases/interfaces/TileEntity";
 import { SimplefiedMovieItem } from "../../aliases/interfaces/movie.types";
-interface TitleData {
-    text: string;
-    isPageTitle: boolean;
-}
-interface TilesListSectionProps {
-    list: OrUndefined<TileEntity[]>;
-    titleData: TitleData;
-};
+import { TilesListSectionProps } from "../../aliases/interfaces/TilesListSectionProps";
+import { nanoid } from "@reduxjs/toolkit";
+
 
 export const TilesListSection = ({ list, titleData }: TilesListSectionProps) => {
     const isMoviesList = (list: TileEntity[]): list is SimplefiedMovieItem[] => {
@@ -25,7 +19,12 @@ export const TilesListSection = ({ list, titleData }: TilesListSectionProps) => 
                     <>
                         <SectionHeader text={titleData.text} setAsPageTitle={titleData.isPageTitle} />
                         <TilesList $moreItems={isMoviesList(list) ? false : true}>
-                            {list?.map((item) => renderVerticalTile(item))}
+                            {list?.map((item) => (
+                                <VerticalTile
+                                    key={nanoid()}
+                                    tileEntity={item}
+                                />
+                            ))}
                         </TilesList>
                     </>
                 )
