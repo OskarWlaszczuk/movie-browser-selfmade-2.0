9024eaa04f6f96ea32b6cec1c698046b8b6e7cb0
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-07-01 10:59:00 +0200

    Refactor useReplaceQueryParameter to use UrlQueryKeyValuePair type for query parameters

--- src/common/hooks/useReplaceQueryParameter.ts
+++ src/common/hooks/useReplaceQueryParameter.ts
@@ -1,15 +1,15 @@
 import { useLocation, useNavigate } from "react-router-dom";
-import { QueryParam } from "../aliases/interfaces/QueryParam";
+import { UrlQueryKeyValuePair } from "../aliases/interfaces/UrlQueryKeyValuePair";
 import { getURLPath } from "../functions/getURLPath";
 
 export const useReplaceQueryParameter = () => {
     const navigate = useNavigate();
     const { pathname, search } = useLocation();
 
-    const replaceQueryParameter = (queryParamsList: QueryParam[]) => {
+    const replaceQueryParameter = (queryParamsList: UrlQueryKeyValuePair[]) => {
         const searchParams = new URLSearchParams(search);
         const isSearchEmpty = !queryParamsList[0].value;
-        const basePath = `${getURLPath(pathname)}/`;
+        const basePath = `/${getURLPath(pathname)}`;
 
         isSearchEmpty ?
             queryParamsList.forEach(({ key }) => {
