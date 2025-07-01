fa49ae94e69699e8d3ad033f49689637f0eab7de
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-07-01 10:58:34 +0200

    Replaced renderHorizontalTile function with component HorizontalTIle. This component renders a horizontal layout tile based on the type of the provided entity details (movie or person). It uses type guards to dynamically determine the correct entity type and configure appropriate props for the reusable Tile component. The movie variant includes release year, genres, rating, and additional entry sections, while the person variant includes basic bio info. This structure allows for easy extensibility to support more entity types in the future.

---
+++ src/features/DetailsPage/components/EntityDetails/HorizontalTile/index.tsx
@@ -0,0 +1,78 @@
+import { DetailedMovieItem } from "../../../../../common/aliases/interfaces/movie.types";
+import { DetailedPersonItem } from "../../../../../common/aliases/interfaces/person.types";
+import { DetailedEntityItem } from "../../../../../common/aliases/types/DetailedEntityItem";
+import { OrUndefined } from "../../../../../common/aliases/types/OrUndefined";
+import { Tile, TileProps } from "../../../../../common/components/Tile";
+import { MovieRating } from "../../../../../common/components/Tile/MovieRating";
+import { MovieEntriesSection } from "../../../../../common/components/TilesListSection/VerticalTile/MovieEntriesSection";
+import { entitiesSingularTypes } from "../../../../../common/constants/entityTypes";
+import { getYear } from "../../../../../common/functions/getYear";
+import { EntityDescription } from "./EntityDescription";
+import { MovieInfoSection } from "./MovieInfoSection";
+import { DetailedPersonInfoSection } from "./DetailedPersonInfoSection";
+
+interface HorizontalTileProps {
+    entityDetails: OrUndefined<DetailedEntityItem>;
+}
+
+export const HorizontalTile = ({ entityDetails }: HorizontalTileProps) => {
+
+    type TilePropsConfig<ItemType extends DetailedEntityItem> = {
+        typeGuard: (item: DetailedEntityItem) => item is ItemType;
+        tileProps: (item: ItemType) => TileProps;
+    };
+
+    type TilePropsConfigs = [
+        TilePropsConfig<DetailedMovieItem>,
+        TilePropsConfig<DetailedPersonItem>,
+    ];
+
+    const tilePropsConfigs: TilePropsConfigs = [
+        {
+            typeGuard: (item): item is DetailedMovieItem => !!item && "production_countries" in item,
+            tileProps: (item) => ({
+                title: item.title,
+                picturePath: item.poster_path,
+                infoContent: (
+                    <>
+                        <MovieInfoSection
+                            movieItem={item}
+                            genresIds={item.genres?.map(({ id }) => id)}
+                            extraContent={<MovieEntriesSection detailedMovieItem={item} />}
+                            subTitle={getYear(item?.release_date)}
+                        />
+                        <MovieRating
+                            voteAverage={item?.vote_average}
+                            voteCount={item?.vote_count}
+                        />
+                    </>
+                ),
+                extraContent: <EntityDescription description={item.overview} />,
+                entityType: entitiesSingularTypes.MOVIE,
+            }),
+        },
+        {
+            typeGuard: (item): item is DetailedPersonItem => !!item && "biography" in item,
+            tileProps: (item) => ({
+                title: item.name,
+                picturePath: item.profile_path,
+                infoContent: <DetailedPersonInfoSection detailedPersonItem={item} />,
+                extraContent: <EntityDescription description={item.biography} />,
+                entityType: entitiesSingularTypes.PERSON,
+            }),
+        },
+    ];
+
+    const horizontalTileProps = (
+        tilePropsConfigs.find(
+            ({ typeGuard }) => typeGuard(entityDetails!) === true
+        )?.tileProps(entityDetails as any)
+    );
+
+    return (
+        <Tile
+            {...horizontalTileProps!}
+            useHorizontalLayout
+        />
+    );
+};
