8d16f283c7cc02e185240f6f1a6bfc71d9630863
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-07-01 11:09:19 +0200

    Simplefied Tile. Now it only handles rendering logic and displaying passed props in specific areas. Removed specific entity type props like movieDetails or personDetails in order to make this component more generic. Renamed props: subTitle to infoContent horizontalLayout to useHorizontalLayout Removed id prop, added detailsRoutePath, useTwoColumnsLayout and extraContent optional props

--- src/common/components/Tile/index.tsx
+++ src/common/components/Tile/index.tsx
@@ -1,69 +1,53 @@
-import { ThemeProvider } from "styled-components";
-import { SimplefiedMovieItem } from "../../aliases/interfaces/movie.types";
-import { DetailedPersonItem } from "../../aliases/interfaces/person.types";
-import { GenresIds } from "../../aliases/types/genre.types";
-import { OrNull } from "../../aliases/types/OrNull";
 import { pictureWidths } from "../../constants/pictureConfigs";
-import { routes } from "../../functions/routes";
-import { GenresList } from "../GenresList";
-import { MetaData } from "../MetaData";
-import { MovieRating } from "../MovieRating";
 import { Picture } from "../Picture";
-import { DetailsWrapper, InfoWrapper, Overview, StyledTile, Title } from "./styled";
+import { DetailsWrapper, ExtraContentWrapper, InfoWrapper, StyledTile, Title } from "./styled";
+import { ReactNode } from "react";
+import { EntitySingularType } from "../../aliases/types/entityTypes.types";
+import { OrUndefined } from "../../aliases/types/OrUndefined";
+import { PicturePath } from "../../aliases/types/PicturePath";
 
-interface ProductionCountry {
-    iso_3166_1: string;
-    name: string;
-};
-interface TileProps {
-    id: number;
-    picture: OrNull<string>;
-    title: string;
-    subTitle?: string | number;
-    horizontalLayout?: boolean;
-    movieDetails?: {
-        genresIds: GenresIds;
-        voteAverage: SimplefiedMovieItem["vote_average"];
-        voteCount: SimplefiedMovieItem["vote_count"];
-        releaseDate?: SimplefiedMovieItem["release_date"];
-        productionCountries?: ProductionCountry[];
-        overview?: SimplefiedMovieItem["overview"];
-    };
-    personDetails?: {
-        biography: DetailedPersonItem["biography"];
-        birthday: DetailedPersonItem["birthday"];
-        placeOfBirth: DetailedPersonItem["place_of_birth"];
-    };
+export interface TileProps {
+    picturePath: PicturePath;
+    title: OrUndefined<string>;
+    entityType: EntitySingularType;
+    detailsRoutePath?: string;
+    infoContent?: ReactNode;
+    extraContent?: ReactNode;
+    useHorizontalLayout?: boolean;
+    useTwoColumnsLayout?: boolean;
 };
 
-export const Tile = ({ id, picture, title, subTitle, horizontalLayout, movieDetails, personDetails }: TileProps) => {
+export const Tile = ({
+    detailsRoutePath,
+    picturePath,
+    title,
+    infoContent,
+    extraContent,
+    entityType,
+    useHorizontalLayout,
+    useTwoColumnsLayout,
+}: TileProps) => {
 
-    const entityType = movieDetails ? "movie" : "person";
     return (
-
-        // <HorizontalTile>
-        //     <Picture picturePath={picture} pictureWidth={pictureWidths.tile} entityType={entityType} entityName={title} />
-        //     <div>
-        //         <InfoWrapper>
-        //             <Title>{title}</Title>
-        //             {subTitle && <MetaData>{subTitle}</MetaData>}
-        //             {movieDetails && <GenresList genresIds={movieDetails.genresIds} />}
-        //         </InfoWrapper>
-        //         {movieDetails && <MovieRating voteAverage={movieDetails?.voteAverage} voteCount={movieDetails?.voteCount} />}
-        //         {movieDetails?.overview && <Overview>{movieDetails.overview}</Overview>}
-        //     </div>
-        // </HorizontalTile>
-
-        <StyledTile $horizontalLayout={horizontalLayout} to={entityType === "movie" ? routes.movieDetails(id) : routes.personDetails(id)}>
-            <Picture picturePath={picture} pictureWidth={pictureWidths.tile} entityType={entityType} entityName={title} />
+        <StyledTile
+            $horizontalLayout={useHorizontalLayout}
+            $twoColumns={useTwoColumnsLayout || useHorizontalLayout}
+            to={detailsRoutePath || ""}
+        >
+            <Picture
+                picturePath={picturePath}
+                pictureWidth={pictureWidths.tile}
+                entityType={entityType}
+                entityName={title}
+            />
             <DetailsWrapper>
                 <InfoWrapper>
-                    <Title $horizontalLayout={horizontalLayout}>{title}</Title>
-                    {subTitle && <MetaData>{subTitle}</MetaData>}
-                    {movieDetails && <GenresList genresIds={movieDetails.genresIds} />}
+                    <Title $horizontalLayout={useHorizontalLayout}>{title}</Title>
+                    {infoContent}
                 </InfoWrapper>
-                {movieDetails && <MovieRating voteAverage={movieDetails?.voteAverage} voteCount={movieDetails?.voteCount} />}
-                {movieDetails?.overview && <Overview>{movieDetails.overview}</Overview>}
+                <ExtraContentWrapper>
+                    {extraContent}
+                </ExtraContentWrapper>
             </DetailsWrapper>
         </StyledTile>
     );
