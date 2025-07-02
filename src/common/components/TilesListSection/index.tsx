0c16672f87e77352d8a50f0505fbc88b1147548e
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-07-02 12:34:46 +0200

    Replaced non generic isMoviesList type guard with isPersonEntityList that checks if passed list has some type of PersonEntity union

--- src/common/components/TilesListSection/index.tsx
+++ src/common/components/TilesListSection/index.tsx
@@ -1,24 +1,22 @@
 import { TilesList } from "./styled";
 import { SectionHeader } from "../SectionHeader";
 import { VerticalTile } from "./VerticalTile";
-import { TileEntity } from "../../aliases/interfaces/TileEntity";
-import { SimplefiedMovieItem } from "../../aliases/interfaces/movie.types";
+import {  PersonEntity, TileEntity } from "../../aliases/interfaces/TileEntity";
 import { TilesListSectionProps } from "../../aliases/interfaces/TilesListSectionProps";
 import { nanoid } from "@reduxjs/toolkit";
 
 
 export const TilesListSection = ({ list, titleData }: TilesListSectionProps) => {
-    const isMoviesList = (list: TileEntity[]): list is SimplefiedMovieItem[] => {
-        return list?.length > 0 && "title" in list[0];
-    };
-
-    return (
+    const isPersonEntityList = (item: TileEntity): item is PersonEntity => (
+        !!item && "name" in item
+    );
+   return (
         <>
             {
                 (list && list.length > 0) && (
                     <>
                         <SectionHeader text={titleData.text} setAsPageTitle={titleData.isPageTitle} />
-                        <TilesList $moreItems={isMoviesList(list) ? false : true}>
+                        <TilesList $moreItems={isPersonEntityList(list?.[0]) ? true : false}>
                             {list?.map((item) => (
                                 <VerticalTile
                                     key={nanoid()}
