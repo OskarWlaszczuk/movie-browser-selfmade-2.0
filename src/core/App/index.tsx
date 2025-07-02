5f132dc43cdca07060fd95bc6140c1aade21878d
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-07-02 20:38:06 +0200

    Refactor routing to use listRoutes for improved consistency and clarity

--- src/core/App/index.tsx
+++ src/core/App/index.tsx
@@ -1,7 +1,7 @@
 import { HashRouter, Navigate, Route, Routes } from "react-router-dom";
 import { Movies } from "../../features/ListPage/components/Movies";
 import { NavigationPanel } from "./components/NavigationPanel";
-import { routes } from "../../common/functions/routes";
+import { listRoutes } from "../../common/functions/routes";
 import { People } from "../../features/ListPage/components/People";
 import { Movie } from "../../features/DetailsPage/components/Movie";
 import { Person } from "../../features/DetailsPage/components/Person";
@@ -11,11 +11,11 @@ export const App = () => {
     <HashRouter>
       <NavigationPanel />
       <Routes>
-        <Route path={routes.movies()} element={<Movies />} />
-        <Route path={routes.people()} element={<People />} />
+        <Route path={listRoutes.movies} element={<Movies />} />
+        <Route path={listRoutes.people} element={<People />} />
         <Route path="/movies/:id" element={<Movie />} />
         <Route path="/people/:id" element={<Person />} />
-        <Route path="*" element={<Navigate to={routes.homepage()} />} />
+        <Route path="*" element={<Navigate to={listRoutes.movies} />} />
       </Routes>
     </HashRouter>
   );
