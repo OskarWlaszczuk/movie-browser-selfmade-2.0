aa612c6ac9b82996868ab14ac15d3668201f98f8
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-07-02 12:41:35 +0200

    Refactor MovieEntriesSection to use inline country formatting based on mobile view

--- src/common/components/TilesListSection/VerticalTile/MovieEntriesSection/index.tsx
+++ src/common/components/TilesListSection/VerticalTile/MovieEntriesSection/index.tsx
@@ -1,8 +1,8 @@
 import { EntityEntriesSection } from "../../../../../features/DetailsPage/components/EntityDetails/HorizontalTile/EntityEntriesSection";
 import { MovieEntries } from "../../../../../features/DetailsPage/types/entityEntries.types";
-import { formatProductionCountries } from "../../../../../features/DetailsPage/utilis/formatProductionCountries";
 import { DetailedMovieItem } from "../../../../aliases/interfaces/movie.types";
 import { OrUndefined } from "../../../../aliases/types/OrUndefined";
+import { useIsMobileXL } from "../../../../hooks/useIsMobileXL";
 
 interface MovieEntriesSectionProps {
     detailedMovieItem: OrUndefined<DetailedMovieItem>;
@@ -10,10 +10,14 @@ interface MovieEntriesSectionProps {
 
 export const MovieEntriesSection = ({ detailedMovieItem }: MovieEntriesSectionProps) => {
     const formattedDate = detailedMovieItem?.release_date?.replaceAll("-", ".") || null;
-    const formattedCountries = formatProductionCountries(detailedMovieItem?.production_countries);
+    const isMobileXL = useIsMobileXL()
+
+    const countriesNames = detailedMovieItem?.production_countries?.map(({ name }) => name).join(", ");
+    const countriesShortcuts = detailedMovieItem?.production_countries?.map(({ iso_3166_1 }) => iso_3166_1).join(", ");
+    const countries = isMobileXL ? countriesShortcuts : countriesNames;
 
     const movieEntries: MovieEntries = [
-        { key: "production", value: formattedCountries },
+        { key: "production", value: countries },
         { key: "release date", value: formattedDate }
     ];
 
