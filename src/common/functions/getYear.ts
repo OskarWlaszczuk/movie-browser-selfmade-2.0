32e4328668f7fa1fe60cd6f5f6a0f17a3c27546d
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-07-02 12:43:14 +0200

    Refactor getYear function to use logical AND for cleaner conditional return

--- src/common/functions/getYear.ts
+++ src/common/functions/getYear.ts
@@ -1,4 +1,4 @@
 import { SimplefiedMovieItem } from "../aliases/interfaces/movie.types";
 import { OrUndefined } from "../aliases/types/OrUndefined";
 
-export const getYear = (rawReleaseDate: OrUndefined<SimplefiedMovieItem["release_date"]>) => rawReleaseDate ? new Date(rawReleaseDate).getFullYear() : undefined;
+export const getYear = (rawReleaseDate: OrUndefined<SimplefiedMovieItem["release_date"]>) => rawReleaseDate && new Date(rawReleaseDate).getFullYear();
