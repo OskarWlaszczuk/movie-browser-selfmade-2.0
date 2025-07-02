af1c1dbedd3405a52c4769b373393f974477eaa1
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-07-02 19:55:02 +0200

    Renamed SharedMovieData alias to BaseMovieData.

--- src/common/aliases/interfaces/movie.types.ts
+++ src/common/aliases/interfaces/movie.types.ts
@@ -3,7 +3,7 @@ import { OrEmpty } from "../types/OrEmpty";
 import { OrNull } from "../types/OrNull";
 import { ProductionCountry } from "./ProductionCountry";
 import { SharedTileEntityData } from "./SharedTileEntityData";
-interface SharedMovieData extends SharedTileEntityData {
+interface BaseMovieData extends SharedTileEntityData {
     backdrop_path: OrNull<string>;
     overview: OrNull<string>;
     poster_path: OrNull<string>;
@@ -13,19 +13,19 @@ interface SharedMovieData extends SharedTileEntityData {
     vote_count: number;
 }
 
-export interface SimplefiedMovieItem extends SharedMovieData {
+export interface SimplefiedMovieItem extends BaseMovieData {
     genre_ids: GenresIds;
 }
 
-export interface DetailedMovieItem extends SharedMovieData {
+export interface DetailedMovieItem extends BaseMovieData {
     production_countries: OrEmpty<ProductionCountry[]>;
     genres: OrEmpty<Genre[]>;
 }
 
-export interface PersonCastMovieItem extends SharedMovieData, SimplefiedMovieItem {
+export interface PersonCastMovieItem extends BaseMovieData, SimplefiedMovieItem {
     character: string;
 }
 
-export interface PersonCrewMovieItem extends SharedMovieData, SimplefiedMovieItem {
+export interface PersonCrewMovieItem extends BaseMovieData, SimplefiedMovieItem {
     department: string;
 }
