588ebe2d52797ddf0045166a92e608ef7892bf4a
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-07-02 12:45:23 +0200

    Refactor EntityEntriesSection to use StyledValue and improve mobile responsiveness

--- src/features/DetailsPage/components/EntityDetails/HorizontalTile/EntityEntriesSection/index.tsx
+++ src/features/DetailsPage/components/EntityDetails/HorizontalTile/EntityEntriesSection/index.tsx
@@ -2,29 +2,34 @@ import { nanoid } from "@reduxjs/toolkit";
 import { MetaData } from "../../../../../../common/components/MetaData";
 import { capitalizeFirstLetter } from "../../../../../../common/functions/capitalizeFirstLetter";
 import { EntityEntries } from "../../../../types/entityEntries.types";
+import { StyledValue } from "./styled";
+import { useIsMobileXL } from "../../../../../../common/hooks/useIsMobileXL";
 
 interface EntityEntriesProps {
     entityEntries: EntityEntries;
 }
 
-export const EntityEntriesSection = ({ entityEntries: detailsEntry }: EntityEntriesProps) => (
-    <section>
-        {
-            detailsEntry.map(({ key, value }) => (
-                (value || (Array.isArray(value) && value.length > 0)) && (
-                    <div key={nanoid()}>
-                        <MetaData>{capitalizeFirstLetter(key)}:{" "}
-                            <span style={{
-                                fontSize: "18px",
-                                fontWeight: 400,
-                                color: "black"
-                            }}>
-                                {value}
-                            </span>
-                        </MetaData>
-                    </div>
-                )
-            ))
-        }
-    </section>
-);
+export const EntityEntriesSection = ({ entityEntries: detailsEntry }: EntityEntriesProps) => {
+    const isMobileXL = useIsMobileXL();
+    const getValueElement = (value: string) => <StyledValue>{value}</StyledValue>;
+
+    return (
+        <section>
+            {
+                detailsEntry.map(({ key, value }) => (
+                    (value || (Array.isArray(value) && value.length > 0)) && (
+                        <div key={nanoid()}>
+                            {
+                                isMobileXL ?
+                                    getValueElement(value) :
+                                    <MetaData>{capitalizeFirstLetter(key)}:{" "}
+                                        {getValueElement(value)}
+                                    </MetaData>
+                            }
+                        </div>
+                    )
+                ))
+            }
+        </section>
+    );
+};
