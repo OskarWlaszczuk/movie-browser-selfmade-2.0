679bb2a0eff22e02dee45f14342de577205d7978
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-07-05 16:55:32 +0200

    Adjust font-size for mobile for StyledSectionHeader and MainPageHeader

--- src/common/components/SectionHeader/styled.ts
+++ src/common/components/SectionHeader/styled.ts
@@ -4,6 +4,10 @@ const sharedHeaderStyles = css`
     font-size: ${({ theme }) => theme.fontSizes.title};
     font-weight: ${({ theme }) => theme.fontWeights.semiBold};
     margin:  0 0 24px;
+
+    @media (max-width: ${({ theme }) => theme.breakpoints.tablesS}) {
+        font-size: ${({ theme }) => theme.fontSizes.l};
+    }
 `;
 
 export const StyledSectionHeader = styled.header`
