545789dcf0649f0da0cfd38761933035b42bc237
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-07-05 16:58:36 +0200

    Wrapped TilesListSection's content into section element.

--- src/common/components/TilesListSection/index.tsx
+++ src/common/components/TilesListSection/index.tsx
@@ -1,7 +1,7 @@
 import { TilesList } from "./styled";
 import { SectionHeader } from "../SectionHeader";
 import { VerticalTile } from "./VerticalTile";
-import {  PersonEntity, TileEntity } from "../../aliases/interfaces/TileEntity";
+import { PersonEntity, TileEntity } from "../../aliases/interfaces/TileEntity";
 import { TilesListSectionProps } from "../../aliases/interfaces/TilesListSectionProps";
 import { nanoid } from "@reduxjs/toolkit";
 
@@ -10,11 +10,11 @@ export const TilesListSection = ({ list, titleData }: TilesListSectionProps) => 
     const isPersonEntityList = (item: TileEntity): item is PersonEntity => (
         !!item && "name" in item
     );
-   return (
+    return (
         <>
             {
                 (list && list.length > 0) && (
-                    <>
+                    <section>
                         <SectionHeader text={titleData.text} setAsPageTitle={titleData.isPageTitle} />
                         <TilesList $moreItems={isPersonEntityList(list?.[0]) ? true : false}>
                             {list?.map((item) => (
@@ -24,7 +24,7 @@ export const TilesListSection = ({ list, titleData }: TilesListSectionProps) => 
                                 />
                             ))}
                         </TilesList>
-                    </>
+                    </section>
                 )
             }
         </>
