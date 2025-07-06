a501797a699ad51957977a012be42bc22d79a5ce
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-07-06 21:24:21 +0200

    Created initial version of upgrated Person componet of person details route. It is temporatily called Person2 to prevent error betwen this component and the old one Person. It uses updated version of useFetchEntityProfile (previously useFetchDetailsPageData). This component's route path is dynamiclly selecting using 'role' dynamic parameter

---
+++ src/features/DetailsPage/components/Person2/index.tsx
@@ -0,0 +1,19 @@
+import { useParams } from "react-router-dom";
+import { useFetchEntityProfile } from "../../hooks/useFetchDetailsPageData";
+import { DetailedPersonItem } from "../../../../common/aliases/interfaces/person.types";
+import { PersonCredits } from "../../types/credits.types";
+
+export const Person2 = () => {
+    const { role, id } = useParams();
+
+    const {
+        details,
+        credits,
+        profileStatuses,
+        profilePausedFlags,
+    } = useFetchEntityProfile<DetailedPersonItem, PersonCredits>("person", id!);
+
+    return (
+        <>Person2 {id}, {role}</>
+    );
+}
