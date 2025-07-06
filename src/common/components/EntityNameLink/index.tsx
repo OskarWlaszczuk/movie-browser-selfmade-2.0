8f1e9e935c8b015f5fd4b7e14f3dc95c921d2c39
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-07-06 10:29:30 +0200

    Added styles for hover state, changed default color and background-color for EntityNameLink

--- src/common/components/EntityNameLink/index.tsx
+++ src/common/components/EntityNameLink/index.tsx
@@ -2,8 +2,8 @@ import { NavLink } from "react-router-dom";
 import styled from "styled-components";
 
 export const EntityNameLink = styled(NavLink)`
-    color: ${({ theme }) => theme.colors.black};
-    background-color: ${({ theme }) => theme.colors.mystic};
+    color: ${({ theme }) => theme.colors.ebb};
+    background-color: ${({ theme }) => theme.colors.abbey};
     padding: 8px 16px;
     list-style: none;
     border-radius: 5px;
@@ -11,6 +11,11 @@ export const EntityNameLink = styled(NavLink)`
     white-space: nowrap;
     font-size:  ${({ theme }) => theme.fontSizes.s};
     text-decoration: none;
+
+    &:hover {
+        filter: brightness(110%);
+        color: ${({ theme }) => theme.colors.white};
+    }
 
     @media (max-width: ${({ theme }) => theme.breakpoints.laptopXS}) {
         font-size: ${({ theme }) => theme.fontSizes.xs};
