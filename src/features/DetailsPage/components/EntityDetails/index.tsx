6db754eb5339219aa0ef81a866e80be7aa95f170
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-07-09 11:29:36 +0200

    temporarily commanted out EntityDetails code

--- src/features/DetailsPage/components/EntityDetails/index.tsx
+++ src/features/DetailsPage/components/EntityDetails/index.tsx
@@ -1,38 +1,47 @@
-import { useCombinedFetchStatus } from "../../../../common/hooks/useCombinedFetchStatus";
-import { useFetchGenres } from "../../../../common/hooks/useFetchGenres";
-import { Main } from "../../../../common/components/Main";
-import { useFetchDetailsPageData } from "../../hooks/useFetchDetailsPageData";
-import { Credits } from "./Credits";
-import { ApiEntityPathSegment } from "../../../../common/aliases/types/apiEndpointPaths.types.ts";
-import { HorizontalTile } from "./HorizontalTile";
+import {useCombinedFetchStatus} from "../../../../common/hooks/useCombinedFetchStatus";
+import {useFetchGenres} from "../../../../common/hooks/useFetchGenres";
+import {Main} from "../../../../common/components/Main";
+import {useFetchEntityProfile} from "../../hooks/useFetchDetailsPageData";
+import {Credits} from "./Credits";
+import {ApiEntityPathSegment} from "../../../../common/aliases/types/apiEndpointPaths.types.ts";
+import {HorizontalTile} from "./HorizontalTile";
+import {EntityListWrapper} from "../../../../common/components/EntityLinksList/styled";
+import {detailsRoutes} from "../../../../common/functions/routes";
+import {EntityLinksList} from "../../../../common/components/EntityLinksList";
+
 interface EntityDetailsProps {
-    entityPathSegment: ApiEntityPathSegment;
+  entityPathSegment: ApiEntityPathSegment;
 }
 
-export const EntityDetails = ({ entityPathSegment }: EntityDetailsProps) => {
-    const genresStatus = useFetchGenres();
+export const EntityDetails = ({entityPathSegment}: EntityDetailsProps) => {
+  // const genresStatus = useFetchGenres();
 
-    const {
-        entityDetails,
-        entityCredits,
-        detailsPageDataStatuses,
-        detailsPausedFlags,
-    } = useFetchDetailsPageData(entityPathSegment);
+  // const {
+  //     entityDetails,
+  //     entityCredits,
+  //     detailsPageDataStatuses,
+  //     detailsPausedFlags,
+  // } = useFetchEntityProfile(entityPathSegment);
 
-    const combinedFetchStatus = useCombinedFetchStatus([...detailsPageDataStatuses, genresStatus], detailsPausedFlags);
+  // const combinedFetchStatus = useCombinedFetchStatus([...detailsPageDataStatuses, genresStatus], detailsPausedFlags);
+  // const cast = entityCredits?.cast?.map(({ name, id }) => ({ name, routePath: `/actor/${id}` }));
+  // const crew = entityCredits?.crew?.map(({ name, id, job }) => ({ name, routePath: `/${job.replaceAll(" ", "-").toLowerCase()}/${id}` }));
 
-    return (
-        <>
-            <Main
-                successContent={
-                    <>
-                        <HorizontalTile entityDetails={entityDetails} />
-                        <Credits credists={entityCredits} />
-                    </>
-                }
-                combinedFetchStatus={combinedFetchStatus}
-                errorMessage="Details not found"
-            />
-        </>
-    );
+  return (
+    // <>
+    //     <Main
+    //         successContent={
+    //             <>
+    //                 <HorizontalTile entityDetails={entityDetails} />
+    //                 {/* <Credits credists={entityCredits} /> */}
+    //                 <EntityLinksList entities={cast!} />
+    //                 <EntityLinksList entities={crew!} />
+    //             </>
+    //         }
+    //         combinedFetchStatus={combinedFetchStatus}
+    //         errorMessage="Details not found"
+    //     />
+    // </>
+    <></>
+  );
 };
