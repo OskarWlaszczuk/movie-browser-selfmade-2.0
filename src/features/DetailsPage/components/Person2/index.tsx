50bbfcc8158d88b365723320acbd8576015b5fa5
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-07-10 08:35:53 +0200

    Refactored the Person2 component to display a dynamic list of movies based on the selected job role from a <select> dropdown. Added support for fetching available job roles using the useFetchJobsConfiguration hook and extracting a unique set of roles from both the cast and crew credits. Introduced a handleChange function to update the URL with the normalized role value upon selection change, enabling route-based synchronization of state. The list of movies is now filtered dynamically based on the selected role, and rendered using the TilesListSection component. Also replaced static content with structured UI elements and integrated the useNavigate hook for programmatic navigation.

--- src/features/DetailsPage/components/Person2/index.tsx
+++ src/features/DetailsPage/components/Person2/index.tsx
@@ -1,19 +1,56 @@
-import { useParams } from "react-router-dom";
-import { useFetchEntityProfile } from "../../hooks/useFetchEntityProfile";
-import { DetailedPersonItem } from "../../../../common/aliases/interfaces/person.types";
-import { PersonCredits } from "../../types/credits.types";
+import {useNavigate, useParams} from "react-router-dom";
+import {useFetchEntityProfile} from "../../hooks/useFetchEntityProfile";
+import {DetailedPersonItem} from "../../../../common/aliases/interfaces/person.types";
+import {PersonCredits} from "../../types/credits.types";
+import {useFetchJobsConfiguration} from "../Movie2";
+import {ChangeEvent} from "react";
+import {TilesListSection} from "../../../../common/components/TilesListSection";
 
 export const Person2 = () => {
-    const { role, id } = useParams();
+  const {role, id} = useParams();
+  const {
+    data: jobsConfiguration,
+    isPaused: isJobsConfigurationPaused,
+    status: jobsConfigurationStatus,
+  } = useFetchJobsConfiguration();
+  const navigate = useNavigate();
 
-    const {
-        details,
-        credits,
-        profileStatuses,
-        profilePausedFlags,
-    } = useFetchEntityProfile<DetailedPersonItem, PersonCredits>("person", id!);
+  const {details, credits, profileStatuses, profilePausedFlags} =
+    useFetchEntityProfile<DetailedPersonItem, PersonCredits>("person", id!);
 
-    return (
-        <>Person2 {id}, {role}</>
-    );
-}
+  const personJobs = [
+    ...new Set(credits?.crew.map(({job}) => job)),
+    ...(!!credits && credits?.cast?.length > 0 ? ["Actor"] : []),
+  ];
+
+  const handleChange = ({target}: ChangeEvent<HTMLSelectElement>) => {
+    const selectedRole = target.value;
+    const formatterRole = selectedRole.toLowerCase().replace(" ", "-");
+    navigate(`/${formatterRole}/${id}`);
+  };
+
+  const movies =
+    role === "actor"
+      ? credits?.cast
+      : [...(credits?.crew || [])].filter(
+          ({job}) => job.toLowerCase().replace(" ", "-") === role
+        );
+
+  return (
+    <>
+      <label>
+        <select name="personJobs" value={role} onChange={handleChange}>
+          {personJobs.map((job) => {
+            return (
+              <option value={job.toLowerCase().replace(" ", "-")}>{job}</option>
+            );
+          })}
+        </select>
+        <TilesListSection
+          list={movies}
+          titleData={{isPageTitle: true, text: "Florence Pugh"}}
+        />
+      </label>
+    </>
+  );
+};
