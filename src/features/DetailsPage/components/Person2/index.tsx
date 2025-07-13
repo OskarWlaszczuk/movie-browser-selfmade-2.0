fb9a373466da855bac9c60d39ba331317f38fa24
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-07-13 20:27:51 +0200

    Filtering movies by genres, moved role switching logic to RoleSwitcher component

--- src/features/DetailsPage/components/Person2/index.tsx
+++ src/features/DetailsPage/components/Person2/index.tsx
@@ -1,50 +1,96 @@
-import {useNavigate, useParams} from "react-router-dom";
-import {useFetchEntityProfile} from "../../hooks/useFetchEntityProfile";
-import {DetailedPersonItem} from "../../../../common/aliases/interfaces/person.types";
-import {PersonCredits} from "../../types/credits.types";
-import {ChangeEvent} from "react";
-import {TilesListSection} from "../../../../common/components/TilesListSection";
+import { useLocation, useParams } from "react-router-dom";
+import { useFetchEntityProfile } from "../../hooks/useFetchEntityProfile";
+import { DetailedPersonItem } from "../../../../common/aliases/interfaces/person.types";
+import { PersonCredits } from "../../types/credits.types";
+import { TilesListSection } from "../../../../common/components/TilesListSection";
+import { useFetchGenres } from "../../../../common/hooks/useFetchGenres";
+import { PersonCrewMovieItem } from "../../../../common/aliases/interfaces/movie.types";
+import { OrUndefined } from "../../../../common/aliases/types/OrUndefined";
+import { actorRole } from "../../../../common/constants/actorRole";
+import { RoleSwitcher } from "./RoleSwitcher";
+import { GenreFilter } from "./GenreFilter";
+import { formatForURL } from "../../../../common/functions/formatForURL";
+import { Main } from "../../../../common/components/Main";
+import { useCombinedFetchStatus } from "../../../../common/hooks/useCombinedFetchStatus";
+
+export const filterMoviesByRole = (
+  crew: OrUndefined<PersonCrewMovieItem[]>,
+  role: string
+) => crew?.filter(
+  ({ job }) => job.toLowerCase().replace(" ", "-") === role
+);
 
 export const Person2 = () => {
-  const {job, id} = useParams();
-  const navigate = useNavigate();
+  const { role, id } = useParams();
+  const { search } = useLocation();
+  const { genres, genresStatus, isGenresPaused } = useFetchGenres();
+  const {
+    details,
+    credits,
+    profileStatuses,
+    profilePausedFlags
+  } = useFetchEntityProfile<DetailedPersonItem, PersonCredits>("person", id!);
 
-  const {details, credits, profileStatuses, profilePausedFlags} =
-    useFetchEntityProfile<DetailedPersonItem, PersonCredits>("person", id!);
+  const genreKey = "genre";
+  const filteredGenresNames = new URLSearchParams(search).getAll(genreKey);
+  const filteredGenresIDs = (
+    genres
+      ?.filter(({ name }) => filteredGenresNames.includes(formatForURL(name)))
+      .map(({ id }) => id)
+  );
 
-  const personJobs = [
-    ...new Set(credits?.crew.map((member) => member.job)),
-    ...(!!credits && credits?.cast?.length > 0 ? ["Actor"] : []),
-  ];
+  const listToDisplay = (
+    role === actorRole ?
+      credits?.cast :
+      filterMoviesByRole(credits?.crew, role!)
+  );
 
-  const handleChange = ({target}: ChangeEvent<HTMLSelectElement>) => {
-    const selectedRole = target.value;
-    const formatterRole = selectedRole.toLowerCase().replace(" ", "-");
-    navigate(`/${formatterRole}/${id}`);
-  };
+  const moviesGroupedByGenres = (
+    filteredGenresIDs
+      ?.map(filteredID => ({
+        genreList: [...listToDisplay?.filter(({ genre_ids }) => genre_ids.includes(filteredID)) || []],
+        genreName: genres?.find(({ id }) => id === filteredID)?.name
+      }))
+  );
 
-  const movies =
-    job === "actor"
-      ? credits?.cast
-      : [...(credits?.crew || [])].filter(
-          (member) => member.job.toLowerCase().replace(" ", "-") === job
-        );
+  const view = (
+    filteredGenresNames.length > 0 ?
+      moviesGroupedByGenres?.map(({ genreList, genreName }) => (
+        <TilesListSection
+          list={genreList}
+          titleData={{ isPageTitle: false, text: genreName! }}
+        />
+      )) :
+      <TilesListSection
+        list={listToDisplay}
+        titleData={{ isPageTitle: true, text: details?.name! }}
+      />
+  );
+
+
+  const combinedFetchStatus = useCombinedFetchStatus(
+    [...profileStatuses, genresStatus],
+    [...profilePausedFlags, isGenresPaused]
+  );
 
   return (
-    <>
-      <label>
-        <select name="personJobs" value={job} onChange={handleChange}>
-          {personJobs.map((personJob) => {
-            return (
-              <option value={personJob.toLowerCase().replace(" ", "-")}>{personJob}</option>
-            );
-          })}
-        </select>
-        <TilesListSection
-          list={movies}
-          titleData={{isPageTitle: true, text: "Florence Pugh"}}
-        />
-      </label>
-    </>
+    <Main
+      combinedFetchStatus={combinedFetchStatus}
+      successContent={
+        <>
+          <>
+            <RoleSwitcher
+              personId={id!}
+              currentRole={role!}
+              credits={credits}
+            />
+            <GenreFilter
+              genres={genres}
+            />
+          </>
+          {view}
+        </>
+      }
+    />
   );
 };
