69e11a38237bc0884691faf42c7a282d3f5f05cd
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-07-17 10:01:59 +0200

    Moved filtering logic to useFilterMovies hook

--- src/features/DetailsPage/components/Person2/index.tsx
+++ src/features/DetailsPage/components/Person2/index.tsx
@@ -14,10 +14,72 @@ import { getYear } from "../../../../common/functions/getYear";
 import { DecadeFilter } from "./DecadeFilter";
 import { MoviesByGenreGrid } from "./MoviesByGenreGrid";
 import { Genre } from "../../../../common/aliases/types/genre.types";
+import { OrUndefined } from "../../../../common/aliases/types/OrUndefined";
+interface FilterProps {
+  movie: any;
+  searchParams?: URLSearchParams;
+  genres?: OrUndefined<Genre[]>;
+  URLRole?: string
+}
+const genreKey = "genre";
+const decadeKey = "decade";
+
+const isMatchingDecadeRange = ({ movie, searchParams }: FilterProps) => {
+  const decadeValue = searchParams.get(decadeKey);
+  if (!searchParams.has(decadeKey)) return true;
+
+  const decadeYear = Number(decadeValue?.replace("s", ""));
+  const movieYear = getYear(movie.release_date) as number;
+
+  return movieYear! >= decadeYear && movieYear <= decadeYear + 9;
+};
+
+const isMachingGenres = ({ movie, genres, searchParams }: FilterProps) => {
+  const filteredGenresNames = searchParams.getAll(genreKey);
+  if (!searchParams?.has(genreKey)) return true;
+
+  const filteredGenresIDs = (
+    genres
+      ?.filter(({ name }) => filteredGenresNames.includes(formatForURL(name)))
+      .map(({ id }) => id)
+  );
+
+  return movie.genre_ids.some(id => filteredGenresIDs?.includes(id));
+};
+
+const isMatchingRole = ({ movie, URLRole }: FilterProps) => (
+  formatForURL(movie.job) === URLRole
+);
+
+interface UseFilteredMovieProps {
+  genres: OrUndefined<Genre[]>;
+  URLRole: string;
+  movies: any[]
+}
+
+const useFilterMovies = ({ genres, URLRole, movies }: UseFilteredMovieProps) => {
+  const { search } = useLocation();
+  const searchParams = new URLSearchParams(search);
+
+  const movieFilters = {
+    isMatchingDecadeRange,
+    isMachingGenres,
+    isMatchingRole,
+  };
+
+  const filteredMovies = (
+    movies.filter(movie => (
+      Object
+        .values(movieFilters)
+        .every(filter => filter({ movie, searchParams, URLRole, genres }))
+    ))
+  );
+
+  return filteredMovies;
+};
 
 export const Person2 = () => {
   const { role, id } = useParams();
-  const { search } = useLocation();
 
   const { genres, genresStatus, isGenresPaused } = useFetchGenres();
   const {
@@ -31,55 +93,22 @@ export const Person2 = () => {
   const castMovies = credits?.cast.map(movie => ({ ...movie, job: "Actor" }));
   const movies = [...crewMovies || [], ...castMovies || []];
 
-  const searchParams = new URLSearchParams(search);
-  const genreKey = "genre";
-  const decadeKey = "decade";
 
-  const filterByDecade = (movie) => {
-    const decadeValue = searchParams.get(decadeKey);
-    if (!searchParams.has(decadeKey)) return true;
-
-    const decadeYear = Number(decadeValue?.replace("s", ""));
-    const movieYear = getYear(movie.release_date) as number;
-
-    return movieYear! >= decadeYear && movieYear <= decadeYear + 9;
-  };
-
-  const filterByGenres = (movie, genres: Genre[]) => {
-    const filteredGenresNames = searchParams.getAll(genreKey);
-    if (!searchParams.has(genreKey)) return true;
-
-    const filteredGenresIDs = (
-      genres
-        ?.filter(({ name }) => filteredGenresNames.includes(formatForURL(name)))
-        .map(({ id }) => id)
-    );
-
-    return movie.genre_ids.some(id => filteredGenresIDs?.includes(id));
-  };
-
-  const filterByRole = (movie) => formatForURL(movie.job) === role;
-
-  const filters = [
-    filterByDecade,
-    filterByGenres,
-    filterByRole,
-  ];
+  const filteredMovies = useFilterMovies({ movies, URLRole: role!, genres });
 
   const combinedFetchStatus = useCombinedFetchStatus(
     [...profileStatuses, genresStatus],
     [...profilePausedFlags, isGenresPaused]
   );
-
+  
   if (!details || !credits || !genres) return null
-  const filtered = movies.filter(movie => filters.every(filter => filter(movie, genres)));
 
   return (
     <PageContainer>
       <KinematographySection>
         <StyledFiltersPanel>
           <RoleSwitcher
-            credits={credits}
+            movies={movies}
           />
           <GenreFilter
             genres={genres}
@@ -87,7 +116,7 @@ export const Person2 = () => {
           <DecadeFilter />
         </StyledFiltersPanel>
         <MoviesByGenreGrid
-          movies={filtered}
+          movies={filteredMovies}
           genres={genres}
         />
       </KinematographySection>
