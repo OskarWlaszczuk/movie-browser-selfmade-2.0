e445288f3268861b02479dd86bd62b4f5519b9f2
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-07-26 09:10:11 +0200

    Renamed prop fullSectionLink to expandedSectionLink, added new required prop extractTileProps

--- src/features/browse/MoviesBrowse/moviesBrowseConfig.ts
+++ src/features/browse/MoviesBrowse/moviesBrowseConfig.ts
@@ -1,26 +1,35 @@
+import { MovieItem } from "../../../common/aliases/interfaces/movie.types";
 import { ListSectionConfig } from "../../../common/aliases/types/ListSectionConfig";
-import { mediaPluralTypes } from "../../../common/constants/entityTypes";
 
-export const moviesBrowseConfig: ListSectionConfig[] = [
+const baseMovieData = (movie: MovieItem) => ({
+    imagePath: movie.poster_path,
+    name: movie.title,
+    detailsRoute: `movie/${movie.id}`,
+})
+
+export const moviesBrowseConfig: ListSectionConfig<MovieItem>[] = [
     {
         key: 'popular',
         title: 'Popular Movies',
         listType: 'popular',
         apiParams: {},
-        fullSectionLink: `${mediaPluralTypes.MOVIES}/?list=popular`,
+        expandedSectionLink: `?list=popular`,
+        extractTileProps: baseMovieData,
     },
     {
         key: 'top-rated',
         title: 'Top Rated',
         listType: 'top_rated',
         apiParams: {},
-        fullSectionLink: `${mediaPluralTypes.MOVIES}/?list=top-rated`,
+        expandedSectionLink: `?list=top-rated`,
+        extractTileProps: baseMovieData,
     },
     {
         key: 'upcoming',
         title: 'Upcoming',
         listType: 'upcoming',
         apiParams: {},
-        fullSectionLink: `${mediaPluralTypes.MOVIES}/?list=upcoming`,
+        expandedSectionLink: `?list=upcoming`,
+        extractTileProps: baseMovieData,
     },
 ];
