8c5717cbd08e4b77e267d24240b2b15a7189ba29
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-07-27 09:42:47 +0200

    Refactor MoviesBrowse and PeopleBrowse components. Moved fetching lists logic into BrowseSections component.

--- src/features/browse/MoviesBrowse/MoviesBrowse.tsx
+++ src/features/browse/MoviesBrowse/MoviesBrowse.tsx
@@ -1,53 +1,31 @@
 import { MovieItem } from "../../../common/aliases/interfaces/movie.types";
 import { mediaSingularTypes } from "../../../common/constants/entityTypes";
-import { BrowseExpandedSection } from "../components/BrowseExpandedSection";
 import { useMoviesBrowseConfig } from "./useMoviesBrowseConfig";
-import { useCombinedFetchStatus } from "../../../common/hooks/useCombinedFetchStatus";
-import { BrowseSections } from "../components/BrowseSections";
-import { useFetchGenres } from "../../../common/hooks/useFetchGenres";
-import { BrowseSectionConfigWithList } from "../../../common/aliases/interfaces/ExtendedListSectionConfig";
 import { useListParam } from "../../../common/hooks/useListParam";
-import { useFetchMediaLists } from "../../../common/hooks/useFetchMediaList";
-import { Main } from "../../../common/components/Main";
-import { extractMovieProps } from "../../../common/functions/extractMovieProps";
+import { BrowseSections } from "../components/BrowseSections";
+import { ExpandedBrowseSection } from "../components/ExpandedBrowseSection";
 
 export const MoviesBrowse = () => {
     const listParam = useListParam();
-
-    const { genres, genresStatus, isGenresPaused } = useFetchGenres();
-    const moviesBrowseConfig = useMoviesBrowseConfig({ genres });
-
-    const {
-        statuses: moviesListsStatuses,
-        pausedFlags: moviesListsPausedFlags,
-        mediaLists: moviesLists
-    } = useFetchMediaLists({ mediaConfig: moviesBrowseConfig, mediaType: mediaSingularTypes.MOVIE });
-
-    const combinedFetchStatus = useCombinedFetchStatus([...moviesListsStatuses, genresStatus], [...moviesListsPausedFlags, isGenresPaused]);
-
-    const mediaBrowseSections: BrowseSectionConfigWithList<MovieItem>[] = moviesBrowseConfig.map((config, index) => ({
-        ...config,
-        mediaList: moviesLists[index]?.results,
-    }));
-
-    const expandedMediaSection = mediaBrowseSections.find(({ key }) => key === listParam);
+    const moviesBrowseConfig = useMoviesBrowseConfig();
 
     const view = (
-        !!listParam && !!expandedMediaSection ?
-            <BrowseExpandedSection<MovieItem>
-                extractTileProps={extractMovieProps}
-                mediaList={expandedMediaSection.mediaList}
-                filtersConfig={expandedMediaSection.expandedSectionFiltersConfig}
-            /> :
+        !!listParam ?
+            <ExpandedBrowseSection
+                mediaConfig={moviesBrowseConfig}
+                listParam={listParam}
+                mediaType={mediaSingularTypes.MOVIE}
+            />
+            :
             <BrowseSections<MovieItem>
-                mediaSections={mediaBrowseSections}
+                mediaConfig={moviesBrowseConfig}
+                mediaType={mediaSingularTypes.MOVIE}
             />
     );
 
     return (
-        <Main
-            successContent={view}
-            combinedFetchStatus={combinedFetchStatus}
-        />
+        <>
+            {view}
+        </>
     );
 };
--- src/features/browse/PeopleBrowse/PeopleBrowse.tsx
+++ src/features/browse/PeopleBrowse/PeopleBrowse.tsx
@@ -1,12 +1,31 @@
 import { PersonItem } from "../../../common/aliases/interfaces/person.types";
+import { BrowseSections } from "../components/BrowseSections";
+import { ExpandedBrowseSection } from "../components/ExpandedBrowseSection";
 import { mediaSingularTypes } from "../../../common/constants/entityTypes";
-import { BrowseOverview } from "../components/Browse";
-import { peopleBrowseConfig } from "./peopleBrowseConfig";
+import { useListParam } from "../../../common/hooks/useListParam";
+import { usePeopleBrowseConfig } from "./usePeopleBrowseConfig";
 
 export const PeopleBrowse = () => {
+    const listParam = useListParam();
+    const peopleBrowseConfig = usePeopleBrowseConfig();
+
+    const view = (
+        !!listParam ?
+            <ExpandedBrowseSection<PersonItem>
+                mediaConfig={peopleBrowseConfig}
+                listParam={listParam}
+                mediaType={mediaSingularTypes.PERSON}
+            />
+            :
+            <BrowseSections<PersonItem>
+                mediaConfig={peopleBrowseConfig}
+                mediaType={mediaSingularTypes.PERSON}
+            />
+    );
+
     return (
         <>
-            <BrowseOverview<PersonItem> mediaConfig={peopleBrowseConfig} mediaType={mediaSingularTypes.PERSON} />
+            {view}
         </>
     );
 };
