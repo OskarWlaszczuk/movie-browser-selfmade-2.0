abc1e9eef6581088c249d59c843966a251ee3ffe
OskarWlaszczuk <oskarwlaszczuk@gmail.com>
2025-07-28 09:39:47 +0200

    Changed styles to ligher for EntityNameLink

--- src/common/components/EntityNameLink/index.tsx
+++ src/common/components/EntityNameLink/index.tsx
@@ -2,23 +2,22 @@ import { NavLink } from "react-router-dom";
 import styled from "styled-components";
 
 export const EntityNameLink = styled(NavLink)`
-    color: ${({ theme }) => theme.colors.ebb};
-    background-color: ${({ theme }) => theme.colors.abbey};
+     color: ${({ theme }) => theme.colors.black};
+    background-color: ${({ theme }) => theme.colors.mystic};
     padding: 8px 16px;
     list-style: none;
     border-radius: 5px;
     text-align: center;
+    text-decoration: none;
     white-space: nowrap;
     font-size:  ${({ theme }) => theme.fontSizes.s};
-    text-decoration: none;
-
-    &:hover {
-        filter: brightness(110%);
-        color: ${({ theme }) => theme.colors.white};
-    }
 
     @media (max-width: ${({ theme }) => theme.breakpoints.laptopXS}) {
         font-size: ${({ theme }) => theme.fontSizes.xs};
         padding: 4px 8px;
+    }
+
+    &:hover {
+        filter: brightness(90%);
     }
 `;
